6a12f910e4b7b6b9a203d968781916bc7c0f749f
Mark Reynolds <mreynolds@bbn.com>
2007-01-25 22:11:35 +0000

    Initial import

---
+++ proto/docert.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <openssl/err.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/pem.h>
+#include <openssl/pkcs12.h>
+#include <openssl/ui.h>
+#include <openssl/safestack.h>
+#ifndef OPENSSL_NO_ENGINE
+#include <openssl/engine.h>
+#endif
+#ifndef OPENSSL_NO_RSA
+#include <openssl/rsa.h>
+#endif
+#include <openssl/bn.h>
+
+static void printex(X509_EXTENSION *ex)
+{
+  X509V3_EXT_METHOD    *meth;
+  AUTHORITY_KEYID      *aki;
+  BASIC_CONSTRAINTS    *bk;
+  STACK_OF(DIST_POINT) *crld;
+  const unsigned char  *uleon;
+  GENERAL_NAMES *gen;
+  GENERAL_NAME  *gen1;
+  DIST_POINT    *point;
+  void *exts;
+  char *leon;
+  int   j;
+  int   k;
+
+  meth = X509V3_EXT_get(ex);
+  //      (void)printf("\tMethod pointer 0x%x\n", meth);
+  if ( meth == NULL )
+    return;
+  uleon = ex->value->data;
+  if ( meth->it )
+    exts = ASN1_item_d2i(NULL, &uleon, ex->value->length,
+			 ASN1_ITEM_ptr(meth->it));
+  else
+    exts = meth->d2i(NULL, &uleon, ex->value->length);
+  //      (void)printf("\tExtension data pointer 0x%x\n", exts);
+  if ( exts == NULL )
+    return;
+  switch ( meth->ext_nid )
+    {
+    case NID_basic_constraints:
+      bk = (BASIC_CONSTRAINTS *)exts;
+      if ( bk->ca == 0 )
+	(void)printf("\t\tCA: FALSE\n");
+      else
+	(void)printf("\t\tCA: TRUE\n");
+      break;
+    case NID_subject_key_identifier:
+      if ( meth->i2s != NULL )
+	{
+	  leon = meth->i2s(meth, exts);
+	  if ( leon != NULL )
+	    {
+	      (void)printf("\t\t%s\n", leon);
+	      OPENSSL_free(leon);
+	    }
+	}
+      break;
+    case NID_authority_key_identifier:
+      aki = (AUTHORITY_KEYID *)exts;
+      if ( aki->keyid != NULL )
+	{
+	  leon = hex_to_string(aki->keyid->data, aki->keyid->length);
+	  if ( leon != NULL )
+	    {
+	      (void)printf("\t\t%s\n", leon);
+	      OPENSSL_free(leon);
+	    }
+	}
+      else
+	{
+	  (void)printf("Certificate with AKI=ISSUER/SNO\n");
+	  (void)printf("This is PROHIBITED by the profile\n");
+	}
+      break;
+    case NID_crl_distribution_points:
+      crld = (STACK_OF(DIST_POINT) *)exts;
+      for(j=0;j<sk_DIST_POINT_num(crld);j++)
+	{
+	  point = sk_DIST_POINT_value(crld, j);
+	  if ( point->distpoint != NULL )
+	    {
+//	      (void)printf("\t\tCRLDP(%d):\n", j+1);
+	      gen = point->distpoint->name.fullname;
+	      for(k=0;k<sk_GENERAL_NAME_num(gen);k++)
+		{
+		  gen1 = sk_GENERAL_NAME_value(gen, k);
+// TODO: must handle wider set of cases here, incl DIRNAME
+		  (void)printf("\t\t%s\n", gen1->d.ia5->data);
+		}
+	    }
+	}
+      break;
+// TODO: SIA and AIA
+    default:
+      break;
+    }
+  if ( meth->it )
+    ASN1_item_free(exts, ASN1_ITEM_ptr(meth->it));
+  else
+    meth->ext_free(exts);
+}
+
+int main(int argc, char **argv)
+{
+  X509_EXTENSION *ex;
+  ASN1_INTEGER   *a1;
+  ASN1_OBJECT    *ao;
+  ASN1_GENERALIZEDTIME *nb4;
+  ASN1_GENERALIZEDTIME *af4;
+  unsigned long   ell;
+  X509_CINF      *ci;
+  BIGNUM *bn;
+  BIO    *cert;
+  X509   *x = NULL;
+  char   *leon;
+  unsigned char *bef;
+  unsigned char *aft;
+  char    buf[256];
+  int     sta;
+  int     excnt;
+  int     i;
+  int     j;
+
+  if ( argc < 2 )
+    return(1);
+  (void)setbuf(stdout, NULL);
+  cert = BIO_new(BIO_s_file());
+  // (void)printf("BIO pointer is 0x%x\n", cert);
+  if ( cert == NULL )
+    return(-1);
+  for(j=1;j<argc;j++)
+    {
+      sta = BIO_read_filename(cert, argv[j]);
+      (void)printf("Status reading %s: %d\n", argv[j], sta);
+      if ( sta <= 0 )
+	continue;
+      x = d2i_X509_bio(cert, NULL);
+      //      (void)printf("X509 pointer is 0x%x\n", x);
+      if ( x == NULL )
+	continue;
+      leon = X509_NAME_oneline(X509_get_issuer_name(x), NULL, 0);
+      (void)printf("Issuer: %s\n", leon);
+      leon = X509_NAME_oneline(X509_get_subject_name(x), NULL, 0);
+      (void)printf("Subject: %s\n", leon);
+      a1 = X509_get_serialNumber(x);
+      bn = ASN1_INTEGER_to_BN(a1, NULL);
+      if ( bn == NULL )
+	{
+	  X509_free(x);
+	  continue;
+	}
+      leon = BN_bn2dec(bn);
+      (void)printf("Serial number: %s\n", leon);
+      BN_free(bn);
+      nb4 = X509_get_notBefore(x);
+      af4 = X509_get_notAfter(x);
+      ASN1_STRING_to_UTF8(&bef, (ASN1_STRING *)nb4);
+      ASN1_STRING_to_UTF8(&aft, (ASN1_STRING *)af4);
+      (void)printf("Validity from %s to %s\n", bef, aft);
+      ell = X509_subject_name_hash(x);
+      (void)printf("Subject name hash: 0x%x\n", ell);
+      (void)printf("Number of extensions: %d\n",
+		   excnt=X509_get_ext_count(x));
+      ci = x->cert_info;
+      //      (void)printf("Certificate info pointer is 0x%x\n", ci);
+      //      (void)printf("Extensions pointer is 0x%x\n", ci->extensions);
+      for(i=0;i<excnt;i++)
+	{
+	  ex = sk_X509_EXTENSION_value(ci->extensions, i);
+      // (void)printf("\tExtension %d: 0x%x\n", i+1, ex);
+	  ao = X509_EXTENSION_get_object(ex);
+	  memset(buf, 0, 256);
+	  sta = OBJ_obj2txt(buf, 256, ao, 0);
+	  if ( sta > 0 && buf[0] != 0 )
+	    {
+	      (void)printf("\tExtension %d: %s\n", i+1, buf);
+//	      if ( strstr(buf, "Key Identifier") != NULL )
+		printex(ex);
+	    }
+	}
+      X509_free(x);
+    }
+  return(0);
+}
---
+++ proto/sqq.c
@@ -0,0 +1,310 @@
+#include <stdio.h>
+#ifdef WINDOWS
+#include <windows.h>
+#else
+#include <memory.h>
+#include <time.h>
+#endif
+#include <sql.h>
+#include <sqlext.h>
+
+#define SQLOK(s) (s == SQL_SUCCESS || s == SQL_SUCCESS_WITH_INFO)
+
+// get generic error message for a statement error
+
+static void heer(SQLHSTMT h, char *what)
+{
+  SQLINTEGER  nep;
+  SQLSMALLINT tl;
+  char errmsg[1024];
+  char state[24];
+
+  memset(errmsg, 0, 1024);
+  SQLGetDiagRec(SQL_HANDLE_STMT, h, 1, (SQLCHAR *)&state[0], &nep,
+                (SQLCHAR *)&errmsg[0], 1024, &tl);
+  (void)printf("%s failed: SQL state %s; error message %s\n",
+	       what, state, errmsg);
+}
+
+// get error message for a connect error
+
+static void heer2(SQLHDBC h, char *what)
+{
+  SQLINTEGER  nep;
+  SQLSMALLINT tl;
+  char errmsg[1024];
+  char state[24];
+
+  memset(errmsg, 0, 1024);
+  SQLGetDiagRec(SQL_HANDLE_DBC, h, 1, (SQLCHAR *)&state[0], &nep,
+                (SQLCHAR *)&errmsg[0], 1024, &tl);
+  (void)printf("%s failed: SQL state %s; error message %s\n",
+	       what, state, errmsg);
+}
+
+// do an SQL statement operation
+
+static void docatalog(SQLHSTMT h, char *stm, char *what)
+{
+  SQLINTEGER  istm;
+  SQLRETURN   ret;
+
+  istm = strlen(stm);
+  ret = SQLExecDirect(h, (SQLCHAR *)stm, istm);
+  if ( SQLOK(ret) )
+    {
+      (void)printf("%s ok\n", what);
+      istm = 0;
+      ret = SQLRowCount(h, &istm);
+      if ( SQLOK(ret) )
+	(void)printf("Rows affected = %d\n", istm);
+    }
+  else
+    heer(h, what);
+}
+
+// drop the custom table
+
+static void drop(SQLHSTMT h)
+{
+  docatalog(h,
+	    "DROP TABLE IF EXISTS my_odbc_net",
+	    "Conditional drop");
+}
+
+// create the custom table
+
+static void create(SQLHSTMT h)
+{
+  docatalog(h,
+	    "CREATE TABLE my_odbc_net(id int, name varchar(20), idb bigint)",
+	    "Create");
+}
+
+// add a variable value pair with a 64-bit timestamp
+
+#ifdef WINDOWS
+
+static void add(SQLHSTMT h, int v1, char *v2)
+{
+  LARGE_INTEGER large;
+  FILETIME ft;
+  char tmp[512];
+
+  GetSystemTimeAsFileTime(&ft);
+  large.LowPart = ft.dwLowDateTime;
+  large.HighPart = ft.dwHighDateTime;
+//      (void)sprintf(tmp, "INSERT INTO my_odbc_net (id, name, idb) VALUES (%d, '%s', %d)",
+  (void)sprintf(tmp, "INSERT INTO my_odbc_net VALUES (%d, '%s', %I64d)",
+                v1, v2, large.QuadPart);
+  docatalog(h, tmp, "Add");
+}
+
+// windows does not have sleep(sec), it has Sleep(ms)
+
+static void sleep(int s)
+{
+  Sleep(s*1000);
+}
+
+#else
+
+static void add(SQLHSTMT h, int v1, char *v2)
+{
+  unsigned long long large;
+  struct timespec tp;
+  char tmp[512];
+
+  clock_gettime(CLOCK_REALTIME, &tp);
+  large = tp.tv_sec;
+  large *= (unsigned long long)1000000000;
+  large += tp.tv_nsec;
+//      (void)sprintf(tmp, "INSERT INTO my_odbc_net (id, name, idb) VALUES (%d, '%s', %d)",
+  (void)sprintf(tmp, "INSERT INTO my_odbc_net VALUES (%d, '%s', %llu)",
+                v1, v2, large);
+  docatalog(h, tmp, "Add");
+}
+
+#endif
+
+// update a column value to another value
+
+static void update(SQLHSTMT h, int v1, int newv1)
+{
+  char tmp[512];
+
+  (void)sprintf(tmp, "UPDATE my_odbc_net SET id=%d WHERE id=%d",
+                newv1, v1);
+  docatalog(h, tmp, "Update");
+}
+
+// count the number of rows in the table
+
+static void count(SQLHSTMT h)
+{
+  SQLRETURN   ret;
+  SQLSMALLINT ire;
+  SQLSMALLINT nlen;
+  SQLSMALLINT dt;
+  SQLUINTEGER csz;
+  SQLINTEGER  iv;
+  SQLSMALLINT ddig;
+  SQLSMALLINT nullo;
+  unsigned long ival = 0;
+  char colo[256];
+
+  docatalog(h, "SELECT COUNT(*) AS TRows FROM my_odbc_net", "Count");
+  ret = SQLNumResultCols(h, &ire);
+  if ( ! SQLOK(ret) )
+    return;
+  (void)printf("Number of result columns = %d\n", ire);
+  ret = SQLDescribeCol(h, 1, (SQLCHAR *)&colo[0], 256, &nlen, &dt, &csz,
+		       &ddig, &nullo);
+  if ( ! SQLOK(ret) )
+    return;
+  (void)printf("Column 1 name %s\n", colo);
+  ret = SQLBindCol(h, 1, SQL_C_ULONG, (SQLPOINTER)&ival,
+		   sizeof(unsigned long), &iv);
+  if ( ! SQLOK(ret) )
+    return;
+  ret = SQLFetch(h);
+  if ( ! SQLOK(ret) )
+    return;
+  (void)printf("Count = %d\n", ival);
+  SQLCloseCursor(h);
+}
+
+// query all rows
+
+static void query(SQLHSTMT h)
+{
+  SQLINTEGER field1;
+  SQLCHAR    field2[256];
+  SQLBIGINT  field3;
+  SQLINTEGER field1len;
+  SQLINTEGER field2len;
+  SQLINTEGER field3len;
+  SQLRETURN  rc;
+
+  docatalog(h, "SELECT * FROM my_odbc_net", "Query");
+  SQLBindCol(h, 1, SQL_C_ULONG, &field1, sizeof(SQLINTEGER), &field1len);
+  SQLBindCol(h, 2, SQL_C_CHAR, field2, 256*sizeof(SQLCHAR), &field2len);
+  SQLBindCol(h, 3, SQL_C_SBIGINT, &field3, sizeof(SQLBIGINT), &field3len);
+  while ( (rc = SQLFetch(h)) != SQL_NO_DATA )
+    {
+      if ( field1len == SQL_NULL_DATA )
+	(void)printf("x");
+      else
+	(void)printf("%d", field1);
+      if ( field2len == SQL_NULL_DATA )
+	(void)printf("\tNULL");
+      else
+	{
+	  field2[field2len] = 0;
+	  (void)printf("\t%s", field2);
+	}
+      if ( field3len == SQL_NULL_DATA )
+	(void)printf("\tx\n");
+      else
+#ifdef WINDOWS
+	(void)printf("\t%I64d\n", field3);
+#else
+      (void)printf("\t%llu\n", field3);
+#endif
+    }
+  SQLCloseCursor(h);
+}
+
+// create a table, add some rows, do an update, a count and a query
+
+int main(void)
+{
+  SQLHENV     henv1 = NULL;
+  SQLHDBC     hdbc1 = NULL;
+  SQLHSTMT    hstmt1 = NULL;
+  SQLRETURN   ret;
+  SQLSMALLINT inret;
+  SQLSMALLINT outret;
+#ifdef WINDOWS
+  static char leon[] = "DRIVER={MySQL ODBC 3.51 Driver};SERVER=localhost;DATABASE=test;UID=root;PASSWORD=password;";
+#else
+//      static char leon[] = "DSN={MyODBC 3.51 Driver DSN};SERVER=localhost;DATABASE=test;USER=root;PASSWORD=password";
+  static char leon[] = "DSN={MyODBC 3.51 Driver DSN};SERVER=localhost;DATABASE=test;USER=mysql";
+#endif
+  char outlen[1024];
+  int  connd = 0;
+
+  ret = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv1);
+  (void)printf("ENV Handle is 0x%x\n", henv1);
+  if ( SQLOK(ret) )
+    {
+      (void)printf("Success opening env handle\n");
+      ret = SQLSetEnvAttr(henv1, SQL_ATTR_ODBC_VERSION,
+			  (SQLPOINTER)SQL_OV_ODBC3, sizeof(int));
+      if ( SQLOK(ret) )
+	{
+	  (void)printf("Success setting v3\n");
+	  ret = SQLAllocHandle(SQL_HANDLE_DBC, henv1, &hdbc1);
+	  (void)printf("DBC Handle is 0x%x\n", hdbc1);
+	  if ( SQLOK(ret) )
+	    {
+	      (void)printf("Success opening dbc handle\n");
+	      inret = strlen(leon);
+	      ret = SQLDriverConnect(hdbc1, NULL, (SQLCHAR *)&leon[0], inret,
+				     (SQLCHAR *)&outlen[0], 1024, &outret, 0);
+	      if ( !SQLOK(ret) )
+		heer2(hdbc1, "connect");
+	      else
+		{
+		  (void)printf("Success connecting to data source\n");
+//                (void)printf("Completed connection string is %s\n", outlen);
+		  sleep(1);
+		  ret = SQLAllocHandle(SQL_HANDLE_STMT, hdbc1, &hstmt1);
+		  if ( SQLOK(ret) )
+		    {
+		      (void)printf("Success allocating statement\n");
+		      ret = SQLSetStmtAttr(hstmt1, SQL_ATTR_NOSCAN,
+					   (SQLPOINTER)SQL_NOSCAN_ON,
+					   SQL_IS_UINTEGER);
+		      if ( SQLOK(ret) )
+			(void)printf("Success turning off param scanning\n");
+		      drop(hstmt1);
+		      create(hstmt1);
+		      add(hstmt1, 10, "fear");
+		      add(hstmt1, 20, "greggreg");
+		      add(hstmt1, 30, "leonburger");
+		      update(hstmt1, 20, 299);
+		      add(hstmt1, 300, "garbow");
+		      add(hstmt1, 303, "martenizing");
+		      count(hstmt1);
+		      sleep(1);
+		      query(hstmt1);
+		    }
+		  connd++;
+		}
+	    }
+	}
+    }
+  sleep(5);
+  if ( hstmt1 != NULL )
+    {
+      SQLFreeHandle(SQL_HANDLE_STMT, hstmt1);
+      hstmt1 = NULL;
+    }
+  if ( connd > 0 )
+    {
+      SQLDisconnect(hdbc1);
+      connd = 0;
+    }
+  if ( hdbc1 != NULL )
+    {
+      SQLFreeHandle(SQL_HANDLE_DBC, hdbc1);
+      hdbc1 = NULL;
+    }
+  if ( henv1 != NULL )
+    {
+      SQLFreeHandle(SQL_HANDLE_ENV, henv1);
+      henv1 = NULL;
+    }
+  return(0);
+}
