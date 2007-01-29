ecd0c40544abe138e04de0ab9c3d22bbef7bdbda
Mark Reynolds <mreynolds@bbn.com>
2007-01-29 18:28:39 +0000

    Initial template for the DB schema config file.

---
+++ db/apki.scm
@@ -0,0 +1,36 @@
+#
+# $Id$
+#
+# Configuration file for the db schema
+#
+
+#
+# .dsn section has the DSN name
+#
+
+.dsn
+
+DSN :
+;
+
+.end
+
+#
+# .tables section has the tables
+#
+
+.tables
+
+apki_cert "CERTIFICATE" :
+;
+
+apki_crl "CRL" :
+;
+
+apki_roa "ROA" :
+;
+
+apki_metadata :
+;
+
+.end
