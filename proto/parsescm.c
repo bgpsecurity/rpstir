45f2a73df29156587727d5f2c107a0f9be7eb151
Mark Reynolds <mreynolds@bbn.com>
2007-01-30 18:32:58 +0000

    Initial check in

---
+++ proto/parsescm.c
@@ -0,0 +1,17 @@
+/*
+  $Id$
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+/*
+  Parse an scm (database schema) file to build a set of table definitions
+  and also the DSN name.
+*/
+
+#include "scm.h"
+
+scm *parsescm(char *fn, int *errp)
+{
+}
