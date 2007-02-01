bf821477d78437d018dc00a02f3c4b8013ebdba6
Mark Reynolds <mreynolds@bbn.com>
2007-02-01 22:19:14 +0000

    Most recent version, still only partial parsing. Might go to a table based approach instead of a dynamically parsed view.

--- proto/scm.h
+++ proto/scm.h
@@ -25,4 +25,26 @@ typedef struct _scm
   Error codes
 */
 
+#define ERR_SCM_NOERR         0
+#define ERR_SCM_COFILE       -1  	/* cannot open file */
+#define ERR_SCM_NOMEM        -2	        /* out of memory */
+#define ERR_SCM_INVALARG     -3	        /* invalid argument */
+#define ERR_SCM_NODSN        -4	        /* no DSN specified */
+#define ERR_SCM_NODIR        -5         /* missing directive */
+#define ERR_SCM_INVALDIR     -6         /* invalid directive */
+#define ERR_SCM_XMOD         -7         /* extra modifiers */
+#define ERR_SCM_NOMOD        -8         /* missing modifier */
+#define ERR_SCM_INVALMOD     -9         /* invalid modifier */
+#define ERR_SCM_NXEOF       -10         /* unexpected EOF */
+#define ERR_SCM_NXDIR       -11	        /* unexpected end of directive */
+#define ERR_SCM_NXSDIR      -12         /* unexpected end of subdirective */
+#define ERR_SCM_INVALSDIR   -13         /* invalid subdirective */
+
+/*
+  Directives
+*/
+
+#define SCM_DIR_DSN           1          /* DSN directive */
+#define SCM_DIR_TABLES        2          /* TABLES directive */
+
 #endif
