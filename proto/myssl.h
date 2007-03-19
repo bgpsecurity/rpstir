83398f5c857292a5e606ffe71cf32a1677476ce9
Mark Reynolds <mreynolds@bbn.com>
2007-03-19 15:22:57 +0000

    Local SSL-related definitions

---
+++ proto/myssl.h
@@ -0,0 +1,23 @@
+#ifndef _MYSSL_H_
+#define _MYSSL_H_
+
+/*
+  $Id$
+*/
+
+#include <openssl/err.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/pem.h>
+#include <openssl/pkcs12.h>
+#include <openssl/ui.h>
+#include <openssl/safestack.h>
+#ifndef OPENSSL_NO_ENGINE
+#include <openssl/engine.h>
+#endif
+#ifndef OPENSSL_NO_RSA
+#include <openssl/rsa.h>
+#endif
+#include <openssl/bn.h>
+
+#endif
