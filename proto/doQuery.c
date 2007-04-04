b342d631d5a3ba017a7e271a58211842b3dd4648
David Montana <dmontana@bbn.com>
2007-04-04 20:46:39 +0000

    updates to query

--- proto/doQuery.c
+++ proto/doQuery.c
@@ -9,7 +9,12 @@
 #include "err.h"
 
 /****************
- * Read all ROA's from DB and print them out
+ * This is the query client, which allows a user to read information
+ * out of the database and the repository.
+ * The standard query is to read all ROA's from the DB and print them out.
+ * However, there are options to read any type of objects, choosing
+ * a variety of different values to display, and filtering based on
+ * a variety of different fields.
  **************/
 
 #define checkErr(test, printArgs...) \
@@ -20,6 +25,62 @@
 
 #define MAX_VALS 20
 #define MAX_CONDS 10
+
+typedef struct _QueryField	/* field to display or filter on */
+{
+  char     *name;		/* name of the field */
+  int      justDisplay;         /* true if not allowed to filter on field */
+  int      forROAs;             /* true if field is used in a ROA */
+  int      forCRLs;             /* true if field is used in a CRL */
+  int      forCerts;            /* true if field is used in a cert */
+  int      sqlType;             /* what type of data to expect from query */
+  int      maxSize;             /* how much space to allocate for response */
+  char     *dbField;            /* if not NULL, use this for query, not name */
+  char     *otherDBField;       /* if not NULL, second field for query */
+  char     *heading;            /* name of column heading to use in printout */
+  int      requiresJoin;        /* do join with dirs table to get dirname */
+  char     *description;        /* one-line description for user help */
+} QueryField;
+
+/* the set of all query fields */
+static QueryField fields[] = {
+  {"filename", 0, 1, 1, 1, SQL_C_CHAR, 256, NULL, NULL, "Filename", 0,
+   "the filename where the data is stored in the repository"},
+  {"pathname", 1, 1, 1, 1, -1, 0, "dirname", "filename", "Pathname", 1,
+   "full pathname (directory plus filename) where the data is stored"},
+  {"dirname", 0, 1, 1, 1, SQL_C_CHAR, 4096, NULL, NULL, "Directory", 1,
+   "the directory in the repository where the data is stored"},
+  {"ski", 0, 1, 0, 1, SQL_C_CHAR, 128, NULL, NULL, "SKI", 0,
+   "subject key identifier"},
+  {"aki", 0, 0, 0, 1, SQL_C_CHAR, 128, NULL, NULL, "AKI", 0,
+   "authority key identifier"},
+  {"asn", 0, 1, 0, 0, SQL_C_ULONG, 8, NULL, NULL, "AS #", 0,
+   "autonomous system number"},
+  {"addrrng", 1, 1, 0, 0, -1, 0, "dirname", "filename", "IP Addr Range", 1,
+   "IP address range"},
+  {"issuer", 0, 0, 0, 1, SQL_C_CHAR, 512, NULL, NULL, "Issuer", 0,
+   "system that issued the cert/crl"},
+  {"valfrom", 0, 0, 0, 1, SQL_C_CHAR, 32, NULL, NULL, "Valid From", 0,
+   "date/time from which the cert is valid"},
+  {"valto", 0, 0, 0, 1, SQL_C_CHAR, 32, NULL, NULL, "Valid To", 0,
+   "date/time to which the cert is valid"},
+  {"last_upd", 0, 0, 1, 0, SQL_C_CHAR, 32, NULL, NULL, "Last Update", 0,
+   "last update time of the CRL"},
+  {"next_upd", 0, 0, 1, 0, SQL_C_CHAR, 32, NULL, NULL, "Next Update", 0,
+   "next update time of the CRL"},
+  {"crlno", 0, 0, 1, 0, SQL_C_ULONG, 8, NULL, NULL, "CRL #", 0,
+   "CRL number"}
+};
+
+static QueryField *findField (char *name)
+{
+  int i;
+  int size = sizeof (fields) / sizeof (fields[0]);
+  for (i = 0; i < size; i++) {
+    if (strcasecmp (name, fields[i].name) == 0) return &fields[i];
+  }
+  return NULL;
+}
 
 static int fillInSrch (scmsrch *srch, int isChar, unsigned int sz,
                        int colno, char *colname)
@@ -45,45 +106,30 @@ static int handleResults (scmcon *conp, scmsrcha *s, int idx)
 
 static int listOptions (char *objectType)
 {
-  if (strcasecmp (objectType, "roa") == 0) {
-    printf ("\nPossible fields to display or use in clauses for a roa:\n");
-    printf ("  filename: the filename where the roa is stored in the repository\n");
-    printf ("  pathname: full pathname (including the filename) where the roa is stored\n");
-    printf ("            (Note: This can be used only for display.)\n");
-    printf ("  ski: subject key identifier for the roa\n");
-    printf ("  asn: autonomous system number referred to by the roa\n");
-    printf ("  addrrng: IP address ranges referred to by the roa\n");
-    printf ("           (Note: This can be used only for display.)\n\n");
-    return 0;
+  int i, j;
+  int size = sizeof (fields) / sizeof (fields[0]);
+  int isROA = strcasecmp (objectType, "roa") == 0;
+  int isCRL = strcasecmp (objectType, "crl") == 0;
+  int isCert = strcasecmp (objectType, "cert") == 0;
+
+  if ((! isROA) && (! isCRL) && (! isCert)) {
+    printf ("\nBad object type, must be roa, cert or crl\n\n");
+    return -1;
   }
-  if (strcasecmp (objectType, "cert") == 0) {
-    printf ("\nPossible fields to display or use in clauses for a cert:\n");
-    printf ("  filename: the filename where the cert is stored in the repository\n");
-    printf ("  pathname: full pathname (including the filename) where the cert is stored\n");
-    printf ("            (Note: This can be used only for display.)\n");
-    printf ("  subject: subject of the cert\n");
-    printf ("  issuer: system that issued the cert\n");
-    printf ("  ski: subject key identifier for the cert\n");
-    printf ("  aki: authority key identifier for the cert\n");
-    printf ("  valfrom: datetime from which valid\n");
-    printf ("  valto: datetime to which valid\n");
-    printf ("  !!!Dont' forget the rest!!!\n\n");
-    return 0;
+  printf ("\nPossible fields to display or use in clauses for a %s:\n",
+          objectType);
+  for (i = 0; i < size; i++) {
+    if ((fields[i].forROAs && isROA) || (fields[i].forCRLs && isCRL) ||
+        (fields[i].forCerts && isCert)) {
+      printf ("  %s: %s\n", fields[i].name, fields[i].description);
+      if (fields[i].justDisplay) {
+        for (j = 0; j < (int)strlen (fields[i].name) + 4; j++) printf (" ");
+        printf ("(Note: This can be used only for display.)\n");
+      }
+    }
   }
-  if (strcasecmp (objectType, "crl") == 0) {
-    printf ("\nPossible fields to display or use in clauses for a crl:\n");
-    printf ("  filename: the filename where the crl is stored in the repository\n");
-    printf ("  pathname: full pathname (including the filename) where the crl is stored\n");
-    printf ("            (Note: This can be used only for display.)\n");
-    printf ("  issuer: system that issued the crl\n");
-    printf ("  last_upd: last update time of the crl\n");
-    printf ("  next_upd: next update time of the crl\n");
-    printf ("  crlno: crl number\n");
-    printf ("  !!!Dont' forget the rest!!!\n\n");
-    return 0;
-  }
-  printf ("Bad object type, must be roa, cert or crl\n");
-  return -1;
+  printf ("\n");
+  return 0;
 }
 
 static int printUsage()
