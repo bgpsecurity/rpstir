11fe2c6b0dec9b9bc2b4b32c1a8b34c222c8895e
David Montana <dmontana@bbn.com>
2007-06-14 19:50:16 +0000

    bug 32: add scripts for performance testing

---
+++ performance/do_pull.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "delete from apki_cert;" | mysql $APKI_DB -u mysql
+echo "delete from apki_roa;" | mysql $APKI_DB -u mysql
+echo "delete from apki_crl;" | mysql $APKI_DB -u mysql
+echo "delete from apki_dir;" | mysql $APKI_DB -u mysql
+echo "update apki_metadata set rootdir=\"${APKI_ROOT}/trunk/performance/REPOSITORY\";" | mysql $APKI_DB -u mysql
+${APKI_ROOT}/trunk/rsync_aur/rsync_pull.sh rsync_perf.config > perf_output
