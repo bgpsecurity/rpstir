3f7d88c174067f725afb07e45437d4340496bba2
David Montana <dmontana@bbn.com>
2007-06-21 14:56:32 +0000

    bug 60: update script and docs

--- performance/do_pull.sh
+++ performance/do_pull.sh
@@ -5,4 +5,5 @@ echo "delete from apki_roa;" | mysql $APKI_DB -u mysql
 echo "delete from apki_crl;" | mysql $APKI_DB -u mysql
 echo "delete from apki_dir;" | mysql $APKI_DB -u mysql
 echo "update apki_metadata set rootdir=\"${APKI_ROOT}/performance/REPOSITORY\";" | mysql $APKI_DB -u mysql
-${APKI_ROOT}/rsync_aur/rsync_pull.sh rsync_perf.config > perf_output
+echo "Running performance test, output in perf_output"
+${APKI_ROOT}/run_scripts/rsync_pull.sh rsync_perf.config > perf_output
