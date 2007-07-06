7e8d5c1b5064144e5589a32f7747ecc28149e593
Mark Reynolds <mreynolds@bbn.com>
2007-07-06 19:32:44 +0000

    Bug 57. Compability fixes for BSD; also some bug 62 security fixes.

--- proto/makeROA.c
+++ proto/makeROA.c
@@ -8,15 +8,15 @@
 int main(int argc, char** argv)
 {
   struct ROA *roa;
-  char filename[128];
+  char filename[256];
   int isPEM;
 
-  checkErr (argc != 4, "Usage: makeRoa configFile outputPrefix pemOrDer\n");
-  checkErr (! roaFromConfig (argv[1], 0, &roa),
+  checkErr(argc != 4, "Usage: makeRoa configFile outputPrefix pemOrDer\n");
+  checkErr(! roaFromConfig (argv[1], 0, &roa),
             "Could not read config from %s\n", argv[1]);
-  isPEM = tolower (argv[3][0]) != 'd';
-  snprintf (filename, 128, "%s.roa.%s", argv[2], isPEM ? "pem" : "der");
-  checkErr (! roaToFile (roa, filename, isPEM ? FMT_PEM : FMT_DER),
+  isPEM = tolower ((int)(argv[3][0])) != 'd';
+  snprintf (filename, sizeof(filename), "%s.roa.%s", argv[2], isPEM ? "pem" : "der");
+  checkErr(! roaToFile (roa, filename, isPEM ? FMT_PEM : FMT_DER),
             "Could not write file: %s\n", filename);
   return 0;
 }
