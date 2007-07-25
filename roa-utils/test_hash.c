98109c7efed2495a7488a90fa44de55657707ce7
Mark Reynolds <mreynolds@bbn.com>
2007-07-25 14:46:45 +0000

    Bug 71. Added copyright notice.

--- roa-utils/test_hash.c
+++ roa-utils/test_hash.c
@@ -1,6 +1,25 @@
 #include "roa_utils.h"
 #include "cryptlib.h"
 
+/* ***** BEGIN LICENSE BLOCK *****
+ * 
+ * BBN Address and AS Number PKI Database/repository software
+ * Verison 1.0
+ * 
+ * US government users are permitted unrestricted rights as
+ * defined in the FAR.  
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, either express or implied.
+ *
+ * Copyright (C) BBN Technologies 2007.  All Rights Reserved.
+ *
+ * Contributor(s):  Charles Gardiner
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/* $Id$ */
+
 int main(int argc, char **argv)
   {
   CRYPT_CONTEXT hashContext;
