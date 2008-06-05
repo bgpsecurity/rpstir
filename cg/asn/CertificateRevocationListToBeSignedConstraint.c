0ef3868ff9e693764e80729b4460cee9f8b79793
Chris Small <csmall@bbn.com>
2008-06-05 13:50:15 +0000

    Real (not stubbed) version of file from Charlie

--- cg/asn/CertificateRevocationListToBeSignedConstraint.c
+++ cg/asn/CertificateRevocationListToBeSignedConstraint.c
@@ -1,7 +1,11 @@
-#include "certificate.h"
+#include "crlv2.h"
 
-int CertificateRevocationListToBeSignedConstraint(struct CertificateToBeSigned *ctbsp)
+int CertificateRevocationListToBeSignedConstraint(
+  struct CertificateRevocationListToBeSigned *ctbsp)
   {
-    // dummy for now
+  long version;
+  int num = num_items(&ctbsp->extensions.self);
+  read_casn_num(&ctbsp->version.self, &version);
+  if (version <= 1 && num > 0) return 0;
   return 1;
  }
