ce79b603ca3bac2ecb9cb30eec52ba5287572044
Chris Small <csmall@bbn.com>
2008-06-16 20:42:41 +0000

    was calling manifestValidate2 with wrong arguments (a character array instead of a badfile array). While there. do general cleanup to make code more readable.

--- roa-utils/test_manifest.c
+++ roa-utils/test_manifest.c
@@ -22,45 +22,57 @@
  * ***** END LICENSE BLOCK ***** */
 
 int main(int argc, char **argv)
-  {
+{
   struct ROA roa;
   struct Certificate *certp;
-  char **badfilespp, *dirp;
+  char *dirp;
+  struct badfile **badfilespp;
+  int err;
 
   ROA(&roa, 0);
-  if (argc < 2) fprintf(stderr, "argv[1] is directory, others are manifests\n");
-  else 
-    {
-    argv++;
-    dirp = *argv++;
-    for (; argv && *argv; argv++)
-      {
-      if (get_casn_file(&roa.self, argv[0], 0) < 0) fprintf(stderr, "Reading roa failed\n");
-      else if (!(certp = (struct Certificate *)member_casn(&roa.content.signedData.certificates.self, 0)))
-          fprintf(stderr, "Couldn't get certificate in roa\n");
-      else 
-        {
-        if (diff_objid(&roa.content.signedData.encapContentInfo.eContentType, id_roa_pki_manifest))
-          fprintf(stderr, "%s not a manifest\n", *argv);
-        else
-          {
-          int err = manifestValidate2(&roa, dirp, &badfilespp);
-          fprintf(stderr, "Checking %s ", *argv);
-          if (!err) fprintf(stderr, "SUCCEEDED\n");
-          else 
-            {
-            fprintf(stderr, "failed.\n");
-            if (badfilespp)
-              {
-              fprintf(stderr,  "    Bad files were:\n");
-              char **bf;
-              for (bf = badfilespp; bf && *bf; bf++)
-                fprintf(stderr, "        %s\n", *bf);
-              }
-            }
-          }
-        }
+  if (argc < 2) {
+      fprintf(stderr, "usage: %s directory manifest [manifest ...]\n");
+      return 0;
+  }
+  argv++;
+  dirp = *argv++;
+  for (; *argv != NULL; argv++) {
+
+      /* read and decode the file */
+      if (get_casn_file(&roa.self, *argv, 0) != 0) {
+	  fprintf(stderr, "Reading roa failed\n");
+	  continue;
       }
-    }
+
+      /* XXX we get this but never do anything with it */
+      certp = (struct Certificate *)
+	  member_casn(&roa.content.signedData.certificates.self, 0);
+      if (certp == NULL) {
+          fprintf(stderr, "Couldn't get certificate in roa\n");
+	  continue;
+      }
+
+      /* see if it has the right content type (manifest) */
+      if (diff_objid(&roa.content.signedData.encapContentInfo.eContentType, 
+		     id_roa_pki_manifest)) {
+          fprintf(stderr, "%s not a manifest\n", *argv);
+	  continue;
+      }
+
+      /* validate the manifest */
+      err = manifestValidate2(&roa, dirp, &badfilespp);
+      fprintf(stderr, "Checking %s ", *argv);
+      if (err != 0) {
+	  fprintf(stderr, "failed.\n");
+	  if (badfilespp != NULL) {
+              fprintf(stderr,  "    Bad files were:\n");
+              struct badfile **bf;
+              for (bf = badfilespp; bf && *bf; bf++)
+		  fprintf(stderr, "        %s\n", (*bf)->fname);
+	  }
+	  continue;
+      }
+      fprintf(stderr, "SUCCEEDED\n");
+  }
   return 0;
-  } 
+} 
