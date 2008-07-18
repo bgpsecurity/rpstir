32c5dc8237b925a328e70589efb105bc34cc4360
Chris Small <csmall@bbn.com>
2008-07-18 17:14:47 +0000

    testall.sh: try adding all of the test objects to the db
    print-cert-addrs.sh: print out the ipv4 addrs used when creating the
    certs (there were some problems with the addr ranges)

---
+++ testcases/print-cert-addrs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# print the ipv4 addresses in the makeC* files to check the hierarchy
+
+for i in makeC*; do 
+    printf "%-10s %s\n" $i: `head -1 $i`
+done
---
+++ testcases/testall.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+# try adding everything to the db
+
+BASE=`cd ..; pwd`
+REPO=$BASE/testing/REPOSITORY/test
+
+echo copying objects to $REPO
+rm -f $REPO/*
+cp C?*.cer R*.roa M*.man L*.crl $REPO
+
+echo initializing database
+../testing/initDB
+
+echo adding root certificate
+$BASE/proto/rcli -y -F C.cer
+
+echo adding objects
+for i in $REPO/*; do
+    echo -n `basename $i`:
+    $BASE/proto/rcli -f $i
+done
