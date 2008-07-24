fbcf576b7be476e25eb3f9a18f9c7163570272a7
Charlie Gardiner <gardiner@bbn.com>
2008-07-24 14:34:16 +0000

    Added manifestValidate for proto/sqhl.c to use

--- roa-lib/roa_validate.c
+++ roa-lib/roa_validate.c
@@ -3,12 +3,12 @@
 */
 
 /* ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * BBN Address and AS Number PKI Database/repository software
  * Version 1.0
- * 
+ *
  * US government users are permitted unrestricted rights as
- * defined in the FAR.  
+ * defined in the FAR.
  *
  * This software is distributed on an "AS IS" basis, WITHOUT
  * WARRANTY OF ANY KIND, either express or implied.
@@ -40,7 +40,7 @@ static int gen_hash(uchar *inbufp, int bsize, uchar *outbufp, int alg)
   memset(hash, 0, 40);
   cryptInit();
   if (alg == 2) cryptCreateContext(&hashContext, CRYPT_UNUSED, CRYPT_ALGO_SHA2);
-  else if (alg == 1) cryptCreateContext(&hashContext, CRYPT_UNUSED, 
+  else if (alg == 1) cryptCreateContext(&hashContext, CRYPT_UNUSED,
     CRYPT_ALGO_SHA);
   else return ERR_SCM_BADALG;
   cryptEncrypt(hashContext, inbufp, bsize);
@@ -53,7 +53,7 @@ static int gen_hash(uchar *inbufp, int bsize, uchar *outbufp, int alg)
   }
 
 int check_sig(struct ROA *rp, struct Certificate *certp)
-  { 
+  {
   CRYPT_CONTEXT pubkeyContext, hashContext;
   CRYPT_PKCINFO_RSA rsakey;
   // CRYPT_KEYSET cryptKeyset;
@@ -73,7 +73,7 @@ int check_sig(struct ROA *rp, struct Certificate *certp)
   buf = (uchar *)calloc(1, bsize);
   encode_casn(&sigInfop->signedAttrs.self, buf);
   *buf = ASN_SET;
-  
+
   cryptInit();
   if (cryptCreateContext(&hashContext, CRYPT_UNUSED, CRYPT_ALGO_SHA2) != 0)
     return ERR_SCM_INVALSIG;
@@ -95,7 +95,7 @@ int check_sig(struct ROA *rp, struct Certificate *certp)
     {
     c++;
     bsize --;
-    } 
+    }
   cryptSetComponent((&rsakey)->n, c, bsize * 8);
   free(buf);
 
@@ -103,7 +103,7 @@ int check_sig(struct ROA *rp, struct Certificate *certp)
   cryptSetComponent((&rsakey)->e, buf, bsize * 8);
   free(buf);
 
-  cryptSetAttributeString(pubkeyContext, CRYPT_CTXINFO_KEY_COMPONENTS, &rsakey, 
+  cryptSetAttributeString(pubkeyContext, CRYPT_CTXINFO_KEY_COMPONENTS, &rsakey,
 			  sizeof(CRYPT_PKCINFO_RSA));
 
   cryptDestroyComponents(&rsakey);
@@ -132,17 +132,17 @@ int check_sig(struct ROA *rp, struct Certificate *certp)
 static void fill_max(uchar *max)
   {
   max[max[1] + 1] |= ((1 << max[2]) - 1);
-  } 
+  }
 
 static int getTime(struct CertificateValidityDate *cvdp, ulong *datep)
   {
   int ansr;
-  if (size_casn(&cvdp->utcTime) == 0) 
+  if (size_casn(&cvdp->utcTime) == 0)
       ansr = read_casn_time(&cvdp->generalTime, datep);
-  else 
+  else
       ansr = read_casn_time(&cvdp->utcTime, datep);
   return ansr;
-  } 
+  }
 
 
 static int check_cert(struct Certificate *certp)
@@ -153,7 +153,7 @@ static int check_cert(struct Certificate *certp)
 
   if (read_casn_num(&certp->toBeSigned.version.self, (long*)&tmp) < 0 ||
     tmp != 2) return ERR_SCM_BADVERS;
-  if (diff_casn(&certtbsp->signature.algorithm, &certp->algorithm.algorithm)) 
+  if (diff_casn(&certtbsp->signature.algorithm, &certp->algorithm.algorithm))
     return ERR_SCM_BADALG;
   if (getTime(&certtbsp->validity.notBefore, &lo) < 0 ||
     getTime(&certtbsp->validity.notAfter, &hi) < 0 || lo >= hi)
@@ -170,8 +170,8 @@ static int check_cert(struct Certificate *certp)
     extp; extp = (struct Extension *)next_of(&extp->self))
     {
     if (!diff_objid(&extp->extnID, id_basicConstraints) &&
-        size_casn(&extp->extnValue.basicConstraints.cA) > 0) 
-        return ERR_SCM_NOTEE; 
+        size_casn(&extp->extnValue.basicConstraints.cA) > 0)
+        return ERR_SCM_NOTEE;
     if (!diff_objid(&extp->extnID, id_subjectKeyIdentifier))
       {
       uchar *ski;
@@ -195,14 +195,14 @@ static int check_fileAndHash(struct FileAndHash *fahp, int ffd)
   lseek(ffd, 0, SEEK_SET);
   contentsp = (uchar *)calloc(1, name_lth + 2);
   if (read(ffd, contentsp, name_lth + 2) != name_lth) err = ERR_SCM_BADFILE;
-  else if ((hash_lth = gen_hash(contentsp, name_lth, contentsp, 2)) < 0) 
+  else if ((hash_lth = gen_hash(contentsp, name_lth, contentsp, 2)) < 0)
     err = ERR_SCM_BADHASH;
   else
     {
     bit_lth = vsize_casn(&fahp->hash);
     uchar *hashp = (uchar *)calloc(1, bit_lth);
     read_casn(&fahp->hash, hashp);
-    if (hash_lth != bit_lth - 1 || memcmp(&hashp[1], contentsp, hash_lth)) 
+    if (hash_lth != bit_lth - 1 || memcmp(&hashp[1], contentsp, hash_lth))
       err = ERR_SCM_BADHASH;
     free(hashp);
     close(ffd);
@@ -210,12 +210,12 @@ static int check_fileAndHash(struct FileAndHash *fahp, int ffd)
   free(contentsp);
   return err;
   }
-   
+
 static struct Attribute *find_attr(struct SignedAttributes *attrsp, char *oidp)
   {
   struct Attribute *attrp;
   for (attrp = (struct Attribute *)member_casn(&attrsp->self, 0);
-    attrp && diff_objid(&attrp->attrType, oidp); 
+    attrp && diff_objid(&attrp->attrType, oidp);
     attrp = (struct Attribute *)next_of(&attrp->self));
   return attrp;
   }
@@ -235,7 +235,7 @@ static int setup_cert_minmax(struct IPAddressOrRangeA *rpAddrRangep, uchar *cmin
     encode_casn(&rpAddrRangep->addressRange.min, cmin);
     encode_casn(&rpAddrRangep->addressRange.max, cmax);
     }
-  else 
+  else
     {
     if (size_casn(&rpAddrRangep->addressPrefix) > fam) return ERR_SCM_INVALFAM;
     encode_casn(&rpAddrRangep->addressPrefix, cmin);
@@ -246,7 +246,7 @@ static int setup_cert_minmax(struct IPAddressOrRangeA *rpAddrRangep, uchar *cmin
   cmax[2] = 0;
   return 0;
   }
- 
+
 static int setup_roa_minmax(struct IPAddress *ripAddrp, uchar *rmin, uchar *rmax, int fam)
   {
   memset(rmin, 0, MINMAXBUFSIZE);
@@ -286,19 +286,19 @@ static int validateIPContents(struct ROAIPAddrBlocks *ipAddrBlockp)
   struct ROAIPAddressFamily *roaipfamp;
   int i = 0, err = 0;
 
-  for (roaipfamp = &ipAddrBlockp->rOAIPAddressFamily; roaipfamp; 
+  for (roaipfamp = &ipAddrBlockp->rOAIPAddressFamily; roaipfamp;
     roaipfamp = (struct ROAIPAddressFamily *)next_of(&roaipfamp->self))
     {
     if (read_casn(&roaipfamp->addressFamily, rfam) < 0 ||
 	rfam[0] != 0 || (rfam[1] != 1 && rfam[1] != 2)) return ERR_SCM_INVALFAM;
-    i = rfam[1]; 
+    i = rfam[1];
     memset(oldmax, 0, sizeof(oldmax));
-    for (roaAddrp = &roaipfamp->addresses.rOAIPAddress; roaAddrp; 
+    for (roaAddrp = &roaipfamp->addresses.rOAIPAddress; roaAddrp;
       roaAddrp = (struct ROAIPAddress *)next_of(&roaAddrp->self))
       {
       if ((err = test_maxLength(roaAddrp)) < 0 ||
         (err = setup_roa_minmax(&roaAddrp->address, rmin, rmax, i)) < 0) return err;
-      if (memcmp(&rmin[3], &oldmax[3], sizeof(rmin) - 3) < 0 || 
+      if (memcmp(&rmin[3], &oldmax[3], sizeof(rmin) - 3) < 0 ||
 	memcmp(&rmax[3], &rmin[3],   sizeof(rmin) - 3) < 0) return ERR_SCM_INVALIPB;
       }
     }
@@ -317,16 +317,16 @@ static int cmsValidate(struct ROA *rp)
     uchar *tbsp;
 
     // check that roa->content->version == 3
-    if (diff_casn_num(&rp->content.signedData.version.self, 3) != 0) 
+    if (diff_casn_num(&rp->content.signedData.version.self, 3) != 0)
 	return ERR_SCM_BADVERS;
 
     // check that roa->content->digestAlgorithms == SHA-256 and NOTHING ELSE
     //   (= OID 2.16.840.1.101.3.4.2.1)
     if (num_items(&rp->content.signedData.digestAlgorithms.self) != 1 ||
-	diff_objid(&rp->content.signedData.digestAlgorithms.digestAlgorithmIdentifier.algorithm, id_sha256)) 
+	diff_objid(&rp->content.signedData.digestAlgorithms.digestAlgorithmIdentifier.algorithm, id_sha256))
 	return ERR_SCM_BADDA;
 
-    if ((num_certs = num_items(&rp->content.signedData.certificates.self)) > 1) 
+    if ((num_certs = num_items(&rp->content.signedData.certificates.self)) > 1)
 	return ERR_SCM_BADNUMCERTS;
 
     if (num_items(&rp->content.signedData.signerInfos.self) != 1)
@@ -375,7 +375,7 @@ static int cmsValidate(struct ROA *rp)
     tbs_lth = readvsize_casn(&rp->content.signedData.encapContentInfo.eContent.self, &tbsp);
 
     // hash it, make sure it's the right length and it matches the digest
-    if (gen_hash(tbsp, tbs_lth, hashbuf, 2) != 32 || 
+    if (gen_hash(tbsp, tbs_lth, hashbuf, 2) != 32 ||
 	memcmp(digestbuf, hashbuf, 32) != 0) {
 	ret =  ERR_SCM_BADHASH;
     }
@@ -397,23 +397,23 @@ static int cmsValidate(struct ROA *rp)
     // check that roa->content->crls == NULL
     if (size_casn(&rp->content.signedData.crls.self) > 0 ||
 	num_items(&rp->content.signedData.signerInfos.self) != 1 ||
-	diff_casn_num(&rp->content.signedData.signerInfos.signerInfo.version.self, 3) != 0) 
+	diff_casn_num(&rp->content.signedData.signerInfos.signerInfo.version.self, 3) != 0)
 	return ERR_SCM_BADVERS;
 
     // check that roa->content->signerInfo.digestAlgorithm == SHA-256
     //   (= OID 2.16.840.1.101.3.4.2.1)
-    if (diff_objid(&rp->content.signedData.signerInfos.signerInfo.digestAlgorithm.algorithm, id_sha256)) 
+    if (diff_objid(&rp->content.signedData.signerInfos.signerInfo.digestAlgorithm.algorithm, id_sha256))
 	return ERR_SCM_BADDA;
 
-    if (size_casn(&rp->content.signedData.signerInfos.signerInfo.unsignedAttrs.self) != 0) 
+    if (size_casn(&rp->content.signedData.signerInfos.signerInfo.unsignedAttrs.self) != 0)
 	return ERR_SCM_BADATTR;
 
-    // check that roa->content->signerInfoStruct->signatureAlgorithm == 
+    // check that roa->content->signerInfoStruct->signatureAlgorithm ==
     //   RSAEncryption (= OID 1.2.240.113549.1.1.1)
-    if (diff_objid(&rp->content.signedData.signerInfos.signerInfo.signatureAlgorithm.algorithm, id_rsadsi_rsaEncryption)) 
+    if (diff_objid(&rp->content.signedData.signerInfos.signerInfo.signatureAlgorithm.algorithm, id_rsadsi_rsaEncryption))
 	return ERR_SCM_INVALSIG;
 
-    // check that the subject key identifier has proper length 
+    // check that the subject key identifier has proper length
     if (vsize_casn(&rp->content.signedData.signerInfos.signerInfo.sid.subjectKeyIdentifier) != 20)
 	return ERR_SCM_INVALSKI;
 
@@ -439,7 +439,7 @@ int manifestValidate2(struct ROA *rp, char *dirp, struct badfile ***badfilesppp)
   struct Certificate *certp;
   struct badfile **badfilespp = (struct badfile **)0;
   char *fname, *path;
-  int numbadfiles = 0, dir_lth, err = 0, ffd, tmp; 
+  int numbadfiles = 0, dir_lth, err = 0, ffd, tmp;
        // do general checks including signature if cert is present
   if ((err = cmsValidate(rp)) < 0) return err;
      // certificate checks
@@ -447,7 +447,7 @@ int manifestValidate2(struct ROA *rp, char *dirp, struct badfile ***badfilesppp)
   certp = (struct Certificate *)member_casn(&rp->content.signedData.certificates.self, 0);
   if ((tmp = check_cert(certp)) < 0) return tmp;
       // other specific manifest checks
-  if (diff_objid(&rp->content.signedData.encapContentInfo.eContentType, 
+  if (diff_objid(&rp->content.signedData.encapContentInfo.eContentType,
     id_roa_pki_manifest)) return ERR_SCM_BADCT;
   manp = &rp->content.signedData.encapContentInfo.eContent.manifest;
   ulong mlo, mhi;
@@ -455,14 +455,14 @@ int manifestValidate2(struct ROA *rp, char *dirp, struct badfile ***badfilesppp)
       read_casn_time(&manp->nextUpdate, &mhi) <= 0 ||
       mlo >= mhi) return ERR_SCM_BADDATES;
       // all checks done.  Get to the details
-  if (dirp && *dirp) 
+  if (dirp && *dirp)
     {
     dir_lth = strlen(dirp) + 1;
     if (dirp[dir_lth - 2] == '/') dir_lth--;
     }
   else dir_lth = 0;
   path = (char *)calloc(1, dir_lth + 1);
-  for (fahp = (struct FileAndHash *)member_casn(&manp->fileList.self, 0); fahp; 
+  for (fahp = (struct FileAndHash *)member_casn(&manp->fileList.self, 0); fahp;
     fahp = (struct FileAndHash *)next_of(&fahp->self))
     {
     int name_lth = vsize_casn(&fahp->file);
@@ -474,11 +474,11 @@ int manifestValidate2(struct ROA *rp, char *dirp, struct badfile ***badfilesppp)
     tmp = 0;
     if ((ffd = open(path, O_RDONLY)) < 0) tmp = ERR_SCM_COFILE;
     else tmp = check_fileAndHash(fahp, ffd);
-    if (tmp < 0)  // add the file to the list 
+    if (tmp < 0)  // add the file to the list
       {
-      if (numbadfiles == 0) 
+      if (numbadfiles == 0)
 	badfilespp = (struct badfile **)calloc(2, sizeof(struct badfile *));
-      else badfilespp = (struct badfile **)realloc(badfilespp, ((numbadfiles + 1) * 
+      else badfilespp = (struct badfile **)realloc(badfilespp, ((numbadfiles + 1) *
         sizeof(struct badfile *)));
       struct badfile *badfilep = (struct badfile *)calloc(1, sizeof(struct badfile));
       badfilespp[numbadfiles++] = badfilep;
@@ -493,9 +493,20 @@ int manifestValidate2(struct ROA *rp, char *dirp, struct badfile ***badfilesppp)
   return err;
   }
 
+int manifestValidate(struct ROA *manp)
+  {
+  int iRes = cmsValidate(manp);
+  if (iRes < 0) return iRes;
+
+  // check that eContentType is id-roa-pki-manifest(= OID 1.2.240.113549.1.9.16.1.26)
+  if (diff_objid(&manp->content.signedData.encapContentInfo.eContentType,
+    id_roa_pki_manifest)) return ERR_SCM_BADCT;
+  return 0;
+  }
+
 int roaValidate(struct ROA *rp)
 {
-  // Make sure that the ROA meets the provisions outlined in 
+  // Make sure that the ROA meets the provisions outlined in
   // Kent/Kong ROA IETF draft
   int iRes = 0;
   long iAS_ID = 0;
@@ -528,7 +539,7 @@ int roaValidate2(struct ROA *rp, uchar *certp)
   struct Certificate cert;
   char *oidp;
   struct ASNumberOrRangeA *asNumRp;
-  struct IPAddressFamilyA *rpAddrFamp; 
+  struct IPAddressFamilyA *rpAddrFamp;
   struct ROAIPAddressFamily *ripAddrFamp;
   struct IPAddressOrRangeA *rpAddrRangep;
   struct ROAIPAddress *roaAddrp;
@@ -551,7 +562,7 @@ int roaValidate2(struct ROA *rp, uchar *certp)
   //  of having been extracted, it is reputable
   /////////////////////////////////////////////////////////////////
   iRes = 0;
-  for (extp = (struct Extension *)&cert.toBeSigned.extensions.extension; 
+  for (extp = (struct Extension *)&cert.toBeSigned.extensions.extension;
     extp && iRes == 0;
     extp = (struct Extension *)next_of(&extp->self))
     {
@@ -559,7 +570,7 @@ int roaValidate2(struct ROA *rp, uchar *certp)
        // if it's the SKID extension
     if (!memcmp(oidp, id_subjectKeyIdentifier, strlen(oidp)))
       {
-      all3 |= 1;      
+      all3 |= 1;
       // Check that roa->envelope->SKI = cert->SKI
       if (diff_casn(&rp->content.signedData.signerInfos.signerInfo.sid.subjectKeyIdentifier,
         (struct casn *)&extp->extnValue.subjectKeyIdentifier) != 0)
@@ -569,11 +580,11 @@ int roaValidate2(struct ROA *rp, uchar *certp)
     else if (!memcmp(oidp, id_pe_autonomousSysNum, strlen(oidp)))
       {
       all3 |= 2;
-      if (read_casn_num(&(rp->content.signedData.encapContentInfo.eContent.roa.asID), 
+      if (read_casn_num(&(rp->content.signedData.encapContentInfo.eContent.roa.asID),
 			&iAS_ID) < 0) iRes = ERR_SCM_INVALASID;
       else
         {  // look in cert
-        for (asNumRp = &extp->extnValue.autonomousSysNum.asnum.asNumbersOrRanges.aSNumberOrRangeA; 
+        for (asNumRp = &extp->extnValue.autonomousSysNum.asnum.asNumbersOrRanges.aSNumberOrRangeA;
           asNumRp; asNumRp = (struct ASNumberOrRangeA *)next_of(&asNumRp->self))
           {
           ii = tag_casn(&asNumRp->self);
@@ -592,14 +603,14 @@ int roaValidate2(struct ROA *rp, uchar *certp)
       rpAddrFamp = &extp->extnValue.ipAddressBlock.iPAddressFamilyA;
       read_casn(&rpAddrFamp->addressFamily, cfam);
       for (ripAddrFamp = &rp->content.signedData.encapContentInfo.eContent.roa.ipAddrBlocks.
-        rOAIPAddressFamily; 
-        iRes == cTRUE && ripAddrFamp; 
+        rOAIPAddressFamily;
+        iRes == cTRUE && ripAddrFamp;
         ripAddrFamp = (struct ROAIPAddressFamily *)next_of(&ripAddrFamp->self))
         {  // find that family in cert
         read_casn(&ripAddrFamp->addressFamily, rfam);
         while (rpAddrFamp && memcmp(cfam, rfam, 2) != 0)
           {
-          if (!(rpAddrFamp = (struct IPAddressFamilyA *)next_of(&rpAddrFamp->self))) 
+          if (!(rpAddrFamp = (struct IPAddressFamilyA *)next_of(&rpAddrFamp->self)))
             iRes = ERR_SCM_INVALIPB;
           else  read_casn(&rpAddrFamp->addressFamily, cfam);
           }
@@ -608,22 +619,22 @@ int roaValidate2(struct ROA *rp, uchar *certp)
           rpAddrRangep = &rpAddrFamp->ipAddressChoice.addressesOrRanges.iPAddressOrRangeA;
           if ((sta=setup_cert_minmax(rpAddrRangep, cmin, cmax, cfam[1])) < 0) iRes = sta;
                // go through all ip addresses in ROA
-          for (roaAddrp = &ripAddrFamp->addresses.rOAIPAddress; roaAddrp && iRes == 0; 
+          for (roaAddrp = &ripAddrFamp->addresses.rOAIPAddress; roaAddrp && iRes == 0;
             roaAddrp = (struct ROAIPAddress *)next_of(&roaAddrp->self))
             {   // set up the limits
-	    if ((sta = setup_roa_minmax(&roaAddrp->address, rmin, rmax, rfam[1])) < 0) 
+	    if ((sta = setup_roa_minmax(&roaAddrp->address, rmin, rmax, rfam[1])) < 0)
                 iRes = sta;
               // go through cert addresses until a high enough one is found
               // i.e. skip cert addresses whose max is below roa's min
-            while (iRes == 0 && rpAddrRangep && 
+            while (iRes == 0 && rpAddrRangep &&
               memcmp(&cmax[2], &rmin[2], sizeof(rmin) - 2) <= 0)
               {
-              if (!(rpAddrRangep = 
+              if (!(rpAddrRangep =
                   (struct IPAddressOrRangeA *)next_of(&rpAddrRangep->self)) ||
-                  setup_cert_minmax(rpAddrRangep, cmin, cmax, cfam[1]) < 0) 
+                  setup_cert_minmax(rpAddrRangep, cmin, cmax, cfam[1]) < 0)
                   iRes = ERR_SCM_INVALIPB;
               }
-            if (rpAddrRangep && iRes == 0)  
+            if (rpAddrRangep && iRes == 0)
               {  // now at cert values at or beyond roa
                   // if roa min is below cert min OR roa max beyond cert max, bail out
               if ((ii = memcmp(&rmin[2], &cmin[2], sizeof(cmin) - 2)) < 0 ||
@@ -632,7 +643,7 @@ int roaValidate2(struct ROA *rp, uchar *certp)
             }
           if (roaAddrp) iRes = ERR_SCM_INVALIPB;
           }
-        }     
+        }
       }
     }
   if (all3 != 7) iRes = ERR_SCM_INVALIPB;
