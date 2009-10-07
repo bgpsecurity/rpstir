920266754bbdf268d672a43fb0b1d9be9d93d43a
David Montana <dmontana@bbn.com>
2009-10-07 14:35:06 +0000

    have the server now read from the rtr tables in the database rather than directly from the roa table

--- server/server.c
+++ server/server.c
@@ -25,8 +25,8 @@
 
 #include "pdu.h"
 #include "socket.h"
-#include "scmf.h"
 #include "err.h"
+#include "rtrUtils.h"
 #include "querySupport.h"
 #include <stdio.h>
 #include <string.h>
@@ -34,72 +34,61 @@
 
 static scm      *scmp = NULL;
 static scmcon   *connect = NULL;
-static scmsrcha *roaSrch = NULL;
-static scmtab   *table = NULL;
+static scmsrcha *fullSrch = NULL;
+static scmtab   *fullTable = NULL;
 
 static int sock;
 static PDU response;
 static IPPrefixData prefixData;
 
-// for now, just for single client
-static uint clientSerialNum = 0;
-static uint lastSerialNum = 0;
-
 /* callback that sends a single address to the client */
-static int sendResponses(scmcon *conp, scmsrcha *s, int numLine) {
-	char *ptr1 = (char *)s->vec[1].valptr, *ptr2, *end;
+static int sendResponse(scmcon *conp, scmsrcha *s, int numLine) {
+	char *ptr1 = (char *)s->vec[1].valptr, *ptr2;
 	conp = conp; numLine = numLine;
-
-	if (! checkValidity((char *)s->vec[2].valptr, 0, scmp, connect)) return -1;
 
 	response.typeSpecificData = &prefixData;
 	prefixData.flags = FLAG_ANNOUNCE;
 	prefixData.dataSource = SOURCE_RPKI;
 	prefixData.asNumber = *((uint *)s->vec[0].valptr);
 
-	while ((end = strchr(ptr1, '\n')) != 0) {
-		fillInPDUHeader(&response, PDU_IPV4_PREFIX, 0);
-		*end = '\0';
-		ptr2 = strchr(ptr1, '/');
-		*ptr2 = '\0';
+	ptr2 = strchr(ptr1, '/');
+	*ptr2 = '\0';
+	// IPv4
+	if (strchr(ptr1, '.')) {
+	  fillInPDUHeader(&response, PDU_IPV4_PREFIX, 0);
+	  uint val = 0;
+	  ptr1 = strtok(ptr1, ".");
+	  while (ptr1) {
+		val = (val << 8) + atoi(ptr1);
+		ptr1 = strtok(NULL, ".");
+	  }
+	  prefixData.ipAddress[0] = val;
+	}
+	// IPv6
+	else {
+	  fillInPDUHeader(&response, PDU_IPV6_PREFIX, 0);
+	  uint i = 0, val = 0, final = 0;
+	  ptr1 = strtok(ptr1, ":");
+	  while (ptr1) {
+		val = (val << 16) + atoi(ptr1);
+		if (final) {
+		  prefixData.ipAddress[i] = val;
+		  val = 0;
+		  i++;
+		}
+		final = ! final;
+		ptr1 = strtok(NULL, ":");
+	  }
+	}
 
-		// IPv4
-		if (strchr(ptr1, '.')) {
-			uint val = 0;
-			ptr1 = strtok(ptr1, ".");
-			while (ptr1) {
-				val = (val << 8) + atoi(ptr1);
-				ptr1 = strtok(NULL, ".");
-			}
-			prefixData.ipAddress[0] = val;
-		}
-		// IPv6
-		else {
-			fillInPDUHeader(&response, PDU_IPV6_PREFIX, 0);
-			uint i = 0, val = 0, final = 0;
-			ptr1 = strtok(ptr1, ":");
-			while (ptr1) {
-				val = (val << 16) + atoi(ptr1);
-				if (final) {
-					prefixData.ipAddress[i] = val;
-					val = 0;
-					i++;
-				}
-				final = ! final;
-				ptr1 = strtok(NULL, ":");
-			}
-		}
-
-		ptr1 = ptr2 + 1;
-		ptr2 = strchr(ptr1, '/');
-		if (ptr2) *ptr2 = '\0';
-		prefixData.prefixLength = atoi(ptr1);
-		prefixData.maxLength = ptr2 ? atoi(ptr2+1) : prefixData.prefixLength;
-		if (writePDU(&response, sock) == -1) {
-			printf("Error writing response\n");
-			return -1;
-		}
-		ptr1 = end + 1;
+	ptr1 = ptr2 + 1;
+	ptr2 = strchr(ptr1, '/');
+	if (ptr2) *ptr2 = '\0';
+	prefixData.prefixLength = atoi(ptr1);
+	prefixData.maxLength = ptr2 ? atoi(ptr2+1) : prefixData.prefixLength;
+	if (writePDU(&response, sock) == -1) {
+	  printf("Error writing response\n");
+	  return -1;
 	}
 
 	return 0;
@@ -109,10 +98,6 @@ int main(int argc, char **argv) {
 	PDU *request;
 	uint serialNum;
 	char msg[1024];
-
-	if (argc != 2) {
-		printf("Usage: server stalenessSpecsFile\n");
-	}
 
 	if ((sock = getServerSocket()) == -1) {
 		printf("Error opening socket\n");
@@ -137,46 +122,25 @@ int main(int argc, char **argv) {
 	checkErr(scmp == NULL, "Cannot initialize database schema\n");
 	connect = connectscm (scmp->dsn, msg, sizeof(msg));
 	checkErr(connect == NULL, "Cannot connect to database: %s\n", msg);
-	connect->mystat.tabname = "roa";
-	table = findtablescm(scmp, "roa");
-	checkErr(table == NULL, "Cannot find table roa\n");
+	serialNum = getLastSerialNumber(connect, scmp);
 
 	// setup up the query if this is the first time
-	// note that the where string is set to only select valid roa's, where
-    //   the definition of valid is given by the staleness specs
-	if (roaSrch == NULL) {
-		QueryField *field;
-		roaSrch = newsrchscm(NULL, 4, 0, 1);
-		field = findField("asn");
-		addcolsrchscm(roaSrch, "asn", field->sqlType, field->maxSize);
-		field = findField("ip_addrs");
-		addcolsrchscm(roaSrch, "ip_addrs", field->sqlType, field->maxSize);
-		field = findField("ski");
-		addcolsrchscm(roaSrch, "ski", field->sqlType, field->maxSize);
-		field = findField("filename");
-		addcolsrchscm(roaSrch, "filename", field->sqlType, field->maxSize);
-		roaSrch->wherestr[0] = 0;
-		parseStalenessSpecsFile(argv[1]);
-		addQueryFlagTests(roaSrch->wherestr, 0);
+	if (fullSrch == NULL) {
+		fullSrch = newsrchscm(NULL, 2, 0, 1);
+		addcolsrchscm(fullSrch, "asn", SQL_C_ULONG, 8);
+		addcolsrchscm(fullSrch, "ip_addr", SQL_C_CHAR, 50);
+		snprintf (fullSrch->wherestr, WHERESTR_SIZE,
+				  "serial_num = %d", serialNum);
+		fullTable = findtablescm(scmp, "rtr_full");
 	}
 
-
-	// update rtr_update table
-	lastSerialNum++;
-	table = findtablescm(scmp, "rtr_update");
-	snprintf(msg, sizeof(msg), "insert into %s values (%d, now());",
-			 table->tabname, lastSerialNum);
-	statementscm(connect, msg);
-
 	// do the query, with callback sending out the responses
-	searchscm (connect, table, roaSrch, NULL,
-			   sendResponses, SCM_SRCH_DOVALUE_ALWAYS, NULL);
+	searchscm (connect, fullTable, fullSrch, NULL,
+			   sendResponse, SCM_SRCH_DOVALUE_ALWAYS, NULL);
 
 	// finish up by sending the end of data PDU
 	fillInPDUHeader(&response, PDU_END_OF_DATA, 0);
 	response.typeSpecificData = &serialNum;
-	serialNum = lastSerialNum;
-	clientSerialNum = lastSerialNum;
 	if (writePDU(&response, sock) == -1) {
 		printf("Error writing end of data\n");
 		return -1;
