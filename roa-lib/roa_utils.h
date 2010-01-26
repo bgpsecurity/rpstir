0c2db381b1254186cd66513118769fd5ea79c0c8
Andrew Chi <achi@bbn.com>
2010-01-26 20:25:06 +0000

    Changed prototype for check_fileAndHash back to previous (correct) version.  Reverts unintended part of commit 1081.

--- roa-lib/roa_utils.h
+++ roa-lib/roa_utils.h
@@ -256,7 +256,8 @@ extern int roaValidate2(struct ROA *r);
       }
 */
 
-int check_fileAndHash(struct FileAndHash *fahp, int fd);
+int check_fileAndHash(struct FileAndHash *fahp, int fd, uchar *inhash,
+		      int inhashlen, int inhashtotlen);
 
 int manifestValidate2(struct ROA *r, char *dir, struct badfile ***badfilesppp);
 
