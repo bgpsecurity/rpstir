2a9505dcd72c393249e653d4128d4f783a45bca8
Karen Sirois <ksirois@bbn.com>
2010-08-11 14:51:41 +0000

    Made a bit more robust Added more comments

--- testcases/tools/run_tc.py
+++ testcases/tools/run_tc.py
@@ -4,14 +4,20 @@
 # open test driver file
 #  -- run through every line in the file and perform the specified function
 #     For BBN this means using the rcli and query programs to insert/delete
-#     certificates from the database or query the database for the cert
-#     cases:
+#     certificates from the repository or query the repository for the cert.
+#     Test driver file language:
 #      PATH - set the certificate path (for all certificates that aren't
 #             specified by full pathname)
 #      ECHO - print string to stdout
-#      ADD - add this certificate into the repository (and verify it was added)
+#      ADD - add this certificate into the repository (and
+#            optionally verify that it was added)
 #      DELETE - delete this certificate from the repository
-#      DISPLAY - query the database for this repository
+#      DISPLAY - query the repository the certificate and report pass or fail
+#                based upon the query results and the expected result (Note
+#                the expected result is passed on the DISPLAY line
+#
+# See Notes in sample test.conf file for more information on how to define
+# a test case.
 
 import os
 import sys
@@ -27,35 +33,40 @@ def err(code, errstring):
     sys.exit(code)
 
 def run_tests(cfgfile):
-    """ run all test cases"""
+    """ Open driver file and run all test cases specified
+    by the file.
+    """
 
     # open test config file
     try:
         if not os.path.exists(cfgfile):
             err(1,("Test configuration file does not exist: %s") % (cfgfile))
 
-        file = open(cfgfile)
-        for line in file:
-            if not line.startswith("#") and len(line) > 1:
-                process_input(line)
+        try:
+            file = open(cfgfile)
+            for line in file:
+                line = line.strip()
+                if not line.startswith("#") and len(line) > 1:
+                    process_input(line)
+        except IOError, e:
+            err(2,('Error Opening file %s, %s' % (cfgfile, str(e))))
 
 
     except IOError:
-#        traceback.print_exc()
         err(1,("File %s does not exist") % (cfgfile))
-        
+         
 
     return
 
 def process_input(line):
     """
-    Process line from input configuration file.
-    split the command into fields separated by space
+    Process a line from the input configuration file.
+    split the line into fields separated by space
     The first field is the command. Subsequent fields are
     based upon the command.
     """
     global certpath
-    line = line.strip()
+
     fields = line.split()
 
     # convert command to upper case to compare
@@ -82,15 +93,19 @@ def process_input(line):
                 expected = False
 
         # attempt to add the certificate to the repository
-        # Note: if the file does not exist we don't attempt to 
-        # add it so no need to query the repository to see if it was added
-        if add_to_repository(cert, trusted) and display:
-            display_cert(cert,expected)
+        # Note: if the file does not exist (add_to_repository returns
+        # false) we don't query the repository to see if it was added
+        fullpath_cert = create_fullpath_cert(cert)
+        if fullpath_cert is not None:
+            if add_to_repository(fullpath_cert, trusted) and display:
+                display_cert(cert,expected)
 
-    # delete certificate from the repository
+    # delete certificate (not fullpath name) from the repository
     elif command == 'DELETE':
         cert = fields[1]
-        del_from_repository(cert)
+        fullpath_cert = create_fullpath_cert(cert)
+        if fullpath_cert is not None:
+            del_from_repository(fullpath_cert)
 
     # echo the line directly from config file 
     elif command == 'ECHO':
@@ -98,16 +113,16 @@ def process_input(line):
         if len(text) > 0:
             print text
         else:
-            print
+            return
 
-    
-    # display cert in database and print pass or fail
+    # query the repository to see if the cert is there, print pass or fail
     # based upon expected results (from command line)
     elif command == 'DISPLAY':
         result = False
         cert = fields[1]
         if fields[2].upper() == 'TRUE':
             result = True
+
         display_cert(cert,result)
 
     elif command == 'PATH':
@@ -116,21 +131,22 @@ def process_input(line):
             if os.path.exists(p):
                 certpath = p
             else:
-                err(2,'Path %s does not exist') % (p)
+                err(2,('Path %s does not exist' % (p)))
         except IOError:
 #            traceback.print_exc()
             err(1,("Path %s does not exist") % (cfgfile))
             
     else:
+        print ('Invalid command: ignoring line %s' % line)
         return
 
     return
 
 def display_cert(cert, result):
     """
-    Query the repository to see if the certificate is there - compare
+    Query the repository to see if the certificate is there
     parse query results looking for cert - 
-     if there and expected result true then print PASS with flags
+     if there and expected result true then print PASS
      if there and expected result false then print FAIL
      if not there and expected result true then print FAIL
      if not there and expected result false then print PASS
@@ -142,11 +158,17 @@ def display_cert(cert, result):
     if cert is None:
         return
 
+    # get the base filename of the cert
+    cert_fname = os.path.basename(cert)
+    if len(cert_fname) <= 0:
+        print ('Filename error with cert %s' % cert)
+        return
+
     command = ('query -t cert -d filename -d flags')
     out, err = exec_command(command)
     if out is not None:
 #        print 'QUERY OUTPUT: %s' % out
-        if cert in out:
+        if cert_fname in out:
             if result:
                 print ("%s: PASS") % (cert)
             else:
@@ -168,24 +190,23 @@ def add_to_repository(cert, trusted):
     BBN softrware case:
       run rcli command to add certificate to database (rcli -y -f cert)
 
-    Returns False if we did not attempt to add certificate
+    Returns False if we did not attempt to add certificate (invalid file name)
             True if we did try to add certificate (note it may have failed
-                 to be added)
+                 to be added but that is what we are testing)
     """
     global verbose
     
     if cert is None:
         return
 
-    fullpath_cert = create_fullpath_cert(cert)
-    if not os.path.exists(fullpath_cert):
-        print ('File Not Found: %s\n') % (fullpath_cert)
+    if not os.path.exists(cert):
+        print ('File Not Found: %s\n') % (cert)
         return (False)
     else:
         if trusted:
-            command = ("rcli -y -F %s") % (fullpath_cert)
+            command = ("rcli -y -F %s") % (cert)
         else:
-            command = ("rcli -y -f %s") % (fullpath_cert)
+            command = ("rcli -y -f %s") % (cert)
         out, err = exec_command(command)
         if verbose and err:
             print err
@@ -195,6 +216,7 @@ def add_to_repository(cert, trusted):
 def del_from_repository(cert):
     """ 
     Delete certificate from the database
+
     BBN software case:
        run rcli command to delete certificate from the database
        rcli -y -d <cert>
@@ -203,8 +225,7 @@ def del_from_repository(cert):
     if cert is None:
         return
 
-    fullpath_cert = create_fullpath_cert(cert)
-    command = ("rcli -y -d %s") % (fullpath_cert)
+    command = ("rcli -y -d %s") % (cert)
     out,err = exec_command(command)
 
     return
@@ -214,8 +235,9 @@ def create_fullpath_cert(cert):
     """
     global certpath
 
-    # if it starts with a full / then it must already be a fullpath name
-    if cert.startswith('/'):
+    # check to see if it is already a full pathname
+    # or a relative pathname
+    if cert.startswith('/') or cert.startswith('../'):
         return cert
 
     if certpath is None:
@@ -242,8 +264,8 @@ def exec_command(cmd):
 
 def main(argv=None):
     
-    usage = """usage: %prog [options] <test.conf> 
-    Run all test cases specified in the test configuration file 
+    usage = """usage: %prog [options] <test.conf>    
+    Run all test cases specified by the test configuration file 
     """
 
     global verbose
@@ -258,14 +280,14 @@ def main(argv=None):
     parser = OptionParser(usage)
     parser.add_option("-q", "--quiet",
                   action="store_false", dest="verbose", default=True,
-                  help="don't print error or output messages from executed commands")
+                  help="don't print error or output messages from executed repository commands")
 
     opts,args = parser.parse_args()
     verbose = opts.verbose
 
     try:
         if len(argv) < 2:
-            parser.error('Missing Test Configuration File')
+            print ('ERROR: Missing Test Configuration File\n')
             parser.print_help()
             sys.exit(-1)
             
