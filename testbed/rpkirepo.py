c6b60d99b4258f8d49d759e03c754aa0d8a750f8
Ryan Caloras <rcaloras@bbn.com>
2010-10-26 15:48:00 +0000

    added additional arguments for CA_Objects without parents. Now contains seperate initilization process for TAs.

--- testbed/rpkirepo.py
+++ testbed/rpkirepo.py
@@ -19,6 +19,10 @@
 from datetime import datetime
 from time import time
 import netaddr
+import base64
+
+OBJECT_PATH = "/objects"
+REPO_PATH = OBJECT_PATH+"/REPOSITORY"
 
 class Factory:
 	def __init__(self, bluePrintName = "", ipv4List = [], ipv6List= [],\
@@ -47,17 +51,61 @@ class Factory:
 		#return CA_object(parent,self)
 		
 class Resource_Block:
-	def __init__(self, allocated = false, range, ca_name, block_id= 0):
-		self.allocated = allocated
+	def __init__(self, range, ca_name, block_id= 0, allocated=False):
 		#integer tuple or netaddr.IPRange
 		self.range = range
 		self.ca_name = ca_name
 		#not sure if a unique identifier is needed yet
 		self.block_id = block_id
-
+		self.allocated = allocated
 
 class CA_Object:
-	def __init__(self, parent, myFactory):
+
+	def __init__(self, myFactory, parent=None):
+
+		#If the certificate is self signed, for example IANA. Then it has no
+		#parent and we must initialize its values accordingly
+		if parent==None:
+			self.nextChildSN = 0
+			self.id = 0
+			self.myFactory = myFactory
+			self.bluePrintName = myfactory
+			self.parent=None
+			self.notBefore = datetime.now()
+			self.notAfter = datetime.fromtimestamp(time()+myfactory.ttl)
+			self.nickName= bluePrintName+str(id)
+			self.commonName = nickName
+			#List initialization
+			self.children = []
+			self.manifests = []
+			self.roas = []
+			self.crl = []
+			self.SIA = REPO_PATH+"/"+commonName
+
+			keyFileName = OBJECT_PATH+"/keys/"+SIA+"/"+nickName+".p15"
+			command_string = "../cg/tools/gen_key "+keyFileName+ " 1024"
+			#Execute the gen_key command
+			os.system(command_string)
+			self.ski = generate_ski(keyFileName)
+			self.aki = ski
+			#For self signed cert we directly pass the resources, no call to parent.allocate
+			self.ResourcesOwned = (myFactory.ipv4List, myFactory.ipv6List, myFactory.asList)
+			parseResources()
+			#Parse out the resources we've been allocated to blocks and strings for our certificate
+			#Create lists for our blocks for sub_allocation to child ca's.
+			self.ip4ResourcesFree = []; self.ip6ResourcesFree = [] ;self.asResourcesFree = []
+			ip4Strings = []; ip6Strings = []; asStrings = []
+			parseResources()
+			#Sort of a special case, we want the top of our repository to be
+			self.path_CA_cert = REPO_PATH+"/"+base64.urlSafe_b64encode(ski)+".cer"
+			self.certificate = SS_Cert(id,commonName,commonName, notBefore,\
+					notAfter,aki,ski,keyFileName,keyFileName, ip4Strings,\
+					ip6Strings, asStrings,path_CA_Cert, "rsync://"+SIA)
+	
+
+			
+	
+		
 		self.nextChildSN = 0
 		self.id = parent.getNextChildSN()
 		self.myFactory = myFactory
@@ -76,7 +124,7 @@ class CA_Object:
 		self.crl = []
 
 		if factory.breakAway:
-			self.SIA = myFactory.serverName + "/"+nickName
+			self.SIA = REPO_PATH+"/"+myFactory.serverName + "/"+nickName
 		else:
 			self.SIA = parent.SIA + "/" +nickName
 			
@@ -85,13 +133,14 @@ class CA_Object:
 		#Keys are placed under a directory of their SIA. For example
 		#IANA's key should be in IANA/IANA.p15, APNIC's, should be in something
 		#like IANA/APNIC1/APNIC1.p15
-		keyFileName = "objects/keys/"+SIA+"/"+nickName+".p15"
+		keyFileName = OBJECT_PATH+"/keys/"+SIA+"/"+nickName+".p15"
 		command_string = "../cg/tools/gen_key "+keyFileName+ " 1024"
 		#Execute the gen_key command
 		os.system(command_string)
 		
 		#set our ski based on the hash of the public key in our keyfile
-		self.ski = result from .p15-> hash(public_key) which is a hex string
+		#result from .p15-> hash(public_key) which is a hex string
+		self.ski = generate_ski(keyFileName)
 		
 		try:
 			#tuple of type([ipv4 netaddrs,...], [ipv6 netaddrs,...], [as_nums(start,end),...])
@@ -100,36 +149,39 @@ class CA_Object:
 			print "Error occured in allocating resources for this CA_object"
 
 		self.AIA = parent.path_CA_cert
-		self.CRLDP = parent.SIA+"/"+crlfileName.... computed by B64(parent.ski)
-		self.path_CA_cert = parent.SIA+"/"B64(my ski)  
+		#crlfileName.... computed by B64(parent.ski)
+		self.CRLDP = parent.SIA+"/"+base64.urlSafe_b64encode(parent.ski)+".crl"
+		self.path_CA_cert = parent.SIA+"/"+base64.urlSafe_b64encode(ski)+".cer"  
 		
 		#Parse out the resources we've been allocated to blocks and strings for our certificate
 		#Create lists for our blocks for sub_allocation to child ca's.
 		self.ip4ResourcesFree = []; self.ip6ResourcesFree = [] ;self.asResourcesFree = []
 		ip4Strings = []; ip6Strings = []; asStrings = []
-		#IPV4
-		for ipRange in ResourcesOwned[0]:
-			#Put the netaddr into a block and add it to our list of free resources
-			self.ip4ResourcesFree.append(Resource_Block(false, ipRange, id))
-			#Get the string representation of our address and add it to the list we'll pass to CA_cert
-			ip4Strings.append(str(ipRange))
-		#IPv6 same deal as above
-		for ipRange in ResourcesOwned[1]:
-			self.ip6ResourcesFree.append(Resource_Block(false, ipRange, id))
-			ip6Strings.append(str(ipRange))
-		#AS numbers
-		for asRange in ResourcesOwned[2]:
-			#Put the integer tuple into a block and add it to our list of free resources
-			self.asResourcesFree.append(Resource_Block(false, asRange, id))
-			#Get the string representation of our address and add it to the list we'll pass to CA_cert
-			asStrings.append(str(asRange[0])+"-"str(asRange[1])))
+		parseResources()
 			
-			
-		self.certificate = CA_Cert(parent.getNextChildSN(),parent.commonName, \
+		self.certificate = CA_Cert(id,parent.commonName, \
 				commonName, notBefore,notAfter,aki,ski,keyFileName, \
 				parent.keyFileName, ip4Strings, ip6Strings, asStrings, \
 				path_CA_Cert, "rsync://"+CRLDP,"rsync://"+SIA,"rsync://"+AIA)
+	
 		
+	def parseResources(self):
+		#IPV4
+		for ipRange in self.ResourcesOwned[0]:
+			#Put the netaddr into a block and add it to our list of free resources
+			self.ip4ResourcesFree.append(Resource_Block(ipRange, self.commonName))
+			#Get the string representation of our address and add it to the list we'll pass to CA_cert
+			self.ip4Strings.append(str(ipRange))
+		#IPv6 same deal as above
+		for ipRange in self.ResourcesOwned[1]:
+			self.ip6ResourcesFree.append(Resource_Block(ipRange, self.commonName))
+			self.ip6Strings.append(str(ipRange))
+		#AS numbers
+		for asRange in self.ResourcesOwned[2]:
+			#Put the integer tuple into a block and add it to our list of free resources
+			self.asResourcesFree.append(Resource_Block(asRange, self.commonName))
+			#Get the string representation of our address and add it to the list we'll pass to CA_cert
+			self.asStrings.append(str(asRange[0])+"-"+str(asRange[1]))
 		
 	def subAllocateIP4(self,iplist):
 		return [netaddr.IPRange("0.0.0.0","0.0.0.255")]
