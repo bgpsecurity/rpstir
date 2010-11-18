d7d290ac304a9a0f5d0433047212bd9e399428fc
Charlie Gardiner <gardiner@bbn.com>
2010-11-18 21:14:15 +0000

    Added inclusion of testrpwork which runs the rpwork stuff by itself.

---
+++ proto/testrpwork.c
@@ -0,0 +1,56 @@
+/*
+  $Id: rcli.c 836 2008-12-29 20:32:04Z cgardiner $
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <getopt.h>
+#include <time.h>
+#include <netdb.h>
+#ifdef __NetBSD__
+#include <netinet/in.h>
+#endif
+#ifdef __FreeBSD__
+#include <netinet/in.h>
+#endif
+
+#include "scm.h"
+#include "scmf.h"
+#include "sqhl.h"
+#include "diru.h"
+#include "myssl.h"
+#include "err.h"
+
+static void fatal(char *msg)
+  {
+  fprintf(stderr, "%s\n", msg);
+  exit(0);
+  }
+
+int main(int argc, char **argv)
+  {
+  scm *scmp = NULL;
+  scmcon *conp = NULL;
+  scmtab *table = NULL;
+  char errMsg[1024];
+
+  if ((scmp = initscm()) == NULL) fatal("Can't initialize database");
+  if ((conp = connectscm(scmp->dsn, errMsg, 1024)) == NULL)
+    fatal("Can't connect");
+  OpenSSL_add_all_algorithms();
+  ERR_load_crypto_strings();
+  if (!(table = findtablescm(scmp, "certificate")))
+    fatal("Can't get table");
+  if (argc != 2) fatal("Wrong number of args");
+  int ansr = read_SKI_blocks(scmp, conp, argv[1], stderr);
+  if (ansr < 0) fprintf(stderr, "Had error %d: %s\n", ansr, err2string(ansr));
+  fatal("Finished");
+  return 0;
+  }
