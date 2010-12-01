f26fc572e36285b11f10e21529b54362e1f4485c
Charlie Gardiner <gardiner@bbn.com>
2010-12-01 18:27:41 +0000

    Modified the generator files to create 2048-bit signatures.  Created 2048-bit keys for all CAs.

--- testcases/make_test_cert.c
+++ testcases/make_test_cert.c
@@ -415,8 +415,8 @@ static int setSignature(struct Certificate *certp, char *keyfile, int bad)
   else
     {
     signature = (uchar *)calloc(1, signatureLength +20);
-    if ((ansr = cryptCreateSignature(signature, 200, &signatureLength, sigKeyContext,
-      hashContext)) != 0) msg = "signing";
+    if ((ansr = cryptCreateSignature(signature, signatureLength+20, 
+      &signatureLength, sigKeyContext, hashContext)) != 0) msg = "signing";
     else if ((ansr = cryptCheckSignature(signature, signatureLength, sigKeyContext,
       hashContext)) != 0) msg = "verifying";
     }
