ac56adde66b24de2c089a46b709e74f3c127cb23
Andrew Chi <achi@bbn.com>
2011-06-02 16:16:56 +0000

    extractValidityDate now has option to force output to be GeneralizedTime

--- cg/tools/extractValidityDate.c
+++ cg/tools/extractValidityDate.c
@@ -20,6 +20,7 @@
 #include <ctype.h>
 #include <unistd.h>
 #include "certificate.h"
+#include "stringutils.h"
 
 static void usage(int argc, char *argv[])
 {
@@ -31,11 +32,13 @@ static void usage(int argc, char *argv[])
 	  "\n"
 	  "Options:\n"
 	  "    -b\tRetrieve notBefore (default is to extract both)\n"
-	  "    -a\tRetrieve notAfter\n",
+	  "    -a\tRetrieve notAfter\n"
+	  "    -g\tAlways print in GeneralizedTime format\n",
 	  argv[0]);
 }
 
-static int fprintDate(FILE *fp, struct CertificateValidityDate *date)
+static int fprintDate(FILE *fp, struct CertificateValidityDate *date,
+		      int gtime)
 {
   int date_len;			/* length of date string */
   char *date_str = NULL;	/* date string */
@@ -55,9 +58,25 @@ static int fprintDate(FILE *fp, struct CertificateValidityDate *date)
   ret = read_casn(&date->self, (unsigned char*)date_str);
   if (ret < date_len) {
     fprintf(stderr, "Read failure: got %d, expected %d bytes\n", ret,date_len);
-  } else {
+    return -2;
+  }
+  
+  if (gtime && strlen(date_str) == 13) { /* UTCTime must be converted */
+    char year_str[3];
+    int year;
+    /* Interpret UTCTime according to
+       http://tools.ietf.org/html/rfc5280#section-4.1.2.5.1 */
+    strncpy(year_str, date_str, 2);
+    year_str[2] = '\0';
+    year = atoi(year_str);
+    if (year >= 50)
+      fprintf(fp, "19%s\n", date_str);
+    else
+      fprintf(fp, "20%s\n", date_str);
+  } else {		/* GeneralizedTime already, or print either */
     fprintf(fp, "%s\n", date_str);
   }
+
   free(date_str);
 
   return 0;
@@ -68,13 +87,14 @@ int main(int argc, char *argv[])
   int c = 0;			/* command line option character */
   int option_notbefore = 0;	/* retrieve notBefore date */
   int option_notafter = 0;	/* retrieve notAfter date */
+  int option_gtime = 0;		/* force GeneralizedTime output */
   const char *file = NULL;	/* certificate file */
   struct Certificate cert;	/* ASN.1 certificate object */
   int ret;			/* return value */
   
   /* Parse command line arguments. */
   opterr = 0;
-  while ((c = getopt (argc, argv, "ba")) != -1) {
+  while ((c = getopt (argc, argv, "bag")) != -1) {
     switch (c)
       {
       case 'b':
@@ -82,6 +102,9 @@ int main(int argc, char *argv[])
 	break;
       case 'a':
 	option_notafter = 1;
+	break;
+      case 'g':
+	option_gtime = 1;
 	break;
       case '?':
 	if (isprint (optopt))
@@ -117,9 +140,9 @@ int main(int argc, char *argv[])
 
   /* Extract dates */
   if (option_notbefore)
-    fprintDate(stdout, &cert.toBeSigned.validity.notBefore);
+    fprintDate(stdout, &cert.toBeSigned.validity.notBefore, option_gtime);
   if (option_notafter)
-    fprintDate(stdout, &cert.toBeSigned.validity.notAfter);
+    fprintDate(stdout, &cert.toBeSigned.validity.notAfter, option_gtime);
 
   /* Clean up. */
   delete_casn(&cert.self);
