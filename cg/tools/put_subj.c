1ad4bbcd35ded23b8522328307b6c8f958b1cc9d
Charlie Gardiner <gardiner@bbn.com>
2011-06-15 18:47:19 +0000

    Split put_sernum into 2 files.

--- cg/tools/put_subj.c
+++ cg/tools/put_subj.c
@@ -9,7 +9,7 @@
 char *msgs[] =
     {
     "Finished %s OK\n",
-    "Usage: filename serialNum\n",
+    "Usage: filename subjname\n",
     "Can't open %s\n",
     "Error reading %n\n",
     "Error writing %s\n",
@@ -18,19 +18,21 @@ char *msgs[] =
 int fatal(int num, char *note)
   {
   printf(msgs[num], note);
-  exit(num);
+  exit(-1);
   }
 
 int main(int argc, char **argv)
   {
   struct Certificate cert;
-  int certnum;
   Certificate(&cert, (ushort)0);
   if (argc < 3) fatal(1, (char *)0);
   if (get_casn_file(&cert.self, argv[1], 0) <= 0) fatal(1, argv[1]);
-  if (sscanf(argv[2], "%d", &certnum) != 1) fatal(2, "serial number");
-  if (write_casn_num(&cert.toBeSigned.serialNumber, certnum) < 0) 
-    fatal(3, "serial number");
+  struct RelativeDistinguishedName *rdnp = (struct RelativeDistinguishedName *)
+    member_casn(&cert.toBeSigned.subject.rDNSequence.self, 0);
+  struct AttributeValueAssertion *avap = (struct AttributeValueAssertion *)
+    member_casn(&rdnp->self, 0);
+  if (write_casn(&avap->value.commonName.printableString, (uchar *)argv[2], 
+    strlen(argv[2])) < 0) fatal(3, "name");
   if (put_casn_file(&cert.self, argv[1], 0) < 0) fatal(3, argv[1]); 
   fatal(0, argv[1]);
   return 0;
