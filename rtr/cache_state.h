c8bf4fa4f4ca8b8c67e3f317c639fa48967eb2b2
David Mandelberg <dmandelb@bbn.com>
2011-11-03 15:32:04 -0400

    header for (global_)cache_state

---
+++ rtr/cache_state.h
@@ -0,0 +1,35 @@
+#ifndef _RTR_CACHE_STATE_H
+#define _RTR_CACHE_STATE_H
+
+#include <pthread.h>
+#include <stdbool.h>
+
+#include "pdu.h"
+
+struct cache_state {
+	cache_nonce_t nonce;
+	serial_number_t serial_number;
+};
+
+struct global_cache_state {
+	struct cache_state cache_state;
+	pthread_rwlock_t lock;
+};
+
+/**
+	\brief Initialize the global cache state.
+
+	Initialize the lock and get the nonce and serial number from the database.
+
+	@return Whether or not the initialization was successful.
+*/
+bool initialize_global_cache_state(struct global_cache_state * state); // TODO: DB connection parameter
+
+/**
+	Update the global cache state from the database.
+
+	@return Whether or not the update was successful.
+*/
+bool update_global_cache_state(struct  global_cache_state * state); // TODO: DB connection parameter
+
+#endif
