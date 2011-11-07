5200ddc70f6421231acf85e22f06ecdbfae6b6d9
David Mandelberg <dmandelb@bbn.com>
2011-11-07 17:41:00 -0500

    add connection_main() and struct connection_main_args

--- rtr/connection.h
+++ rtr/connection.h
@@ -7,4 +7,13 @@ typedef sem_t cxn_semaphore_t;
 
 enum cxn_state {READY, RUNNING};
 
+struct connection_main_args {
+	int socket;
+	cxn_semaphore_t * semaphore;
+	Queue * db_request_queue;
+	Bag * db_semaphores_all;
+	struct global_cache_state * global_cache_state;
+};
+void * connection_main(void * args_voidp);
+
 #endif
