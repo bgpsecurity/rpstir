b57147cde1a5bab190d0201a853f75e39b56e09d
David Mandelberg <dmandelb@bbn.com>
2011-11-07 17:43:11 -0500

    global_cache_state should have been a pointer, and add the definition for connection_control_main()

--- rtr/connection_control.h
+++ rtr/connection_control.h
@@ -7,7 +7,8 @@ struct connection_control_main_args {
 	int listen_fd;
 	Queue * db_request_queue;
 	Bag * db_semaphores_all;
-	struct global_cache_state global_cache_state;
+	struct global_cache_state * global_cache_state;
 };
+void * connection_control_main(void * args_voidp);
 
 #endif
