fa826fc63825fa82c08c8f7f8f2adb46a5f78a87
David Mandelberg <dmandelb@bbn.com>
2011-11-08 16:59:21 -0500

    more config parameters

--- rtr/config.h
+++ rtr/config.h
@@ -1,6 +1,16 @@
 #ifndef _RTR_CONFIG_H
 #define _RTR_CONFIG_H
 
+
+#define LOG_FILE "rtrd.log"
+#define LOG_FACILITY "rtrd"
+
+#define LISTEN_PORT 1234
+
+#define MAIN_LOOP_INTERVAL 60
+
 #define DB_RESPONSE_BUFFER_LENGTH 3
+#define DB_INITIAL_THREADS 8
+
 
 #endif
