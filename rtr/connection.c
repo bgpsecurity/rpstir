d86b4ea5e37086a9ec2e05d3f1126e7a70866b26
David Mandelberg <dmandelb@bbn.com>
2011-11-14 11:04:28 -0500

    remove a TODO that's been done

--- rtr/connection.c
+++ rtr/connection.c
@@ -17,15 +17,6 @@
 
 #define LOG_PREFIX "[connection] "
 
-/*
-TODO:
-
-Make sure that all resources are cleaned up by functions passed to pthread_cleanup_push, except for the resources that are given by the connection control thread.
-
-Make sure that cleanup ensures no other threads (e.g. db threads) have access to the connection semaphore.
-
-Make sure that cleanup cancels any DB requests.
-*/
 
 struct run_state {
 	int fd;
