89110dadfabd6241ead26fe3288418181e0af4dd
David Mandelberg <dmandelb@bbn.com>
2011-11-14 18:33:01 -0500

    change some values and add comments

--- rtr/config.h
+++ rtr/config.h
@@ -7,14 +7,26 @@
 
 #define LISTEN_PORT 1234
 
-#define MAIN_LOOP_INTERVAL 60
+#define MAIN_LOOP_INTERVAL 5
 
 #define DB_RESPONSE_BUFFER_LENGTH 3
 #define DB_INITIAL_THREADS 8
 
-#define CXN_CACHE_STATE_INTERVAL 10
+/*
+Quote from draft-ietf-sidr-rpki-rtr-19, Section 6.2:
+	The cache MUST rate limit Serial Notifies to no more frequently
+	than one per minute.
+*/
+#define CXN_CACHE_STATE_INTERVAL 60
 
-#define MAX_PDU_SIZE 65536
+// The largest PDU should be an error report PDU.
+// The second largest is an IPv6 prefix at 32 bytes.
+// Error report PDUs MUST NOT contain other error report PDUs,
+// so the largest error report PDU is 8 (header) + 4 (field length)
+// + 32 (IPv6 prefix PDU) + 4 (field length) + length of error text
+// = 48 + length of error text. I.e. 1024 should almost definitely
+// be large enough and is probably significantly larger than necessary.
+#define MAX_PDU_SIZE 1024
 
 
 #endif
