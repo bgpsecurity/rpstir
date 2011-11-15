525845d95479bd7bfb9f5d8262aa904e0b53faf2
David Mandelberg <dmandelb@bbn.com>
2011-11-15 14:15:32 -0500

    more work on testing

--- subsystemTests/rtr/runRTRTest.sh
+++ subsystemTests/rtr/runRTRTest.sh
@@ -1,38 +1,44 @@
-#!/bin/bash
+#!/bin/sh -e
 
 cd "`dirname "$0"`"
 
 . ../../envir.setup
-. "$RPKI_ROOT/trap_errors"
 
 
 SERVER="$RPKI_ROOT/rtr/rtrd"
-CLIENT="$RPKI_ROOT/rtr/rtr-client"
+CLIENT="$RPKI_ROOT/rtr/rtr-test-client"
+LOGS="rtrd.log rtr-test-client.log"
 PORT=1234
+
+
+rm -f $LOGS
 
 
 "$SERVER" &
 SERVER_PID=$!
+sleep 1
+
+cleanup () {
+	kill $SERVER_PID
+	wait $SERVER_PID
+}
 
 compare () {
-	name="`shift`"
+	name="$1"
 	printf >&2 "comparing \"%s\" to \"%s\"... " "$name" "$name.correct"
-	diff -u "$name" "$name.correct" > "$name.diff"
-	if test -s "$name.diff"; then
+	if diff -uN "$name" "$name.correct" > "$name.diff" 2>/dev/null; then
+		echo >&2 "success."
+	else
 		echo >&2 "failed!"
 		echo >&2 "See \"$name.diff\" for the differences."
+		cleanup
 		exit 1
-	else
-		echo >&2 "success."
 	fi
 }
 
-"$CLIENT" send | nc localhost "$PORT" | "$CLIENT" recv <<__EOF__
-reset
-serial 0 5
-__EOF__
+cat commands | "$CLIENT" send | nc localhost "$PORT" | "$CLIENT" recv
+for LOG in $LOGS; do
+	compare "$LOG"
+done
 
-compare rtr-client.log
-
-kill $SERVER_PID
-wait $SERVER_PID
+cleanup
