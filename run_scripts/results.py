97bf1d85c640ebb72b2bb535828c6c6f78522ffa
David Mandelberg <dmandelb@bbn.com>
2011-11-23 19:17:43 -0500

    more blocks

--- run_scripts/results.py
+++ run_scripts/results.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python
-# ***** BEGIN LICENSE BLOCK *****
-#  
-#  BBN Address and AS Number PKI Database/repository software
-#  Version 3.0-beta
-#  
-#  US government users are permitted unrestricted rights as
-#  defined in the FAR.  
-# 
-#  This software is distributed on an "AS IS" basis, WITHOUT
-#  WARRANTY OF ANY KIND, either express or implied.
-# 
-#  Copyright (C) Raytheon BBN Technologies Corp. 2007-2010.  All Rights Reserved.
-# 
-#  Contributor(s):  Andrew Chi
-# 
-# ***** END LICENSE BLOCK *****
 
 # results.py
 #
