fd7d437852c3dde4b2d4251c59477b0422879af4
David Mandelberg <dmandelb@bbn.com>
2011-11-26 20:38:03 -0500

    start work on making erroneous PDUs

--- subsystemTests/rtr/badPDUs.py
+++ subsystemTests/rtr/badPDUs.py
@@ -2,8 +2,40 @@
 
 import sys
 
+
 bad_PDUs_hex = [
-	'0a 0a 0a',
+	# generic errors
+	'01 02 00 00 00 00 00 08', # invalid protocol version = 1, otherwise valid Reset Query
+	'00 ff 00 00 00 00 00 08', # invalid PDU type, otherwise valid Reset Query
+	'00 00 f0 0f ff ff ff ff', # length too long TODO: this should try to send 0xFFFFFFFF bytes, maybe
+	# TODO: PDUs with truncated headers?
+
+	# Serial Notify
+	'00 00 f0 0f 00 00 00 08', # length too short
+	'00 00 f0 0f 00 00 00 0b f0 0f f0', # length too short
+	'00 00 f0 0f 00 00 00 0d f0 0f f0 0f f0', # length too long
+
+	# Serial Query
+	'00 01 f0 0f 00 00 00 08', # length too short
+	'00 01 f0 0f 00 00 00 0b f0 0f f0', # length too short
+	'00 01 f0 0f 00 00 00 0d f0 0f f0 0f f0', # length too long
+
+	# Reset Query
+	'00 02 00 00 00 00 00 09 f0', # length too long
+
+	# TODO: Cache Response
+
+	# TODO: IPv4 Prefix
+
+	# TODO: IPv6 Prefix
+
+	# TODO: End of Data
+
+	# TODO: Cache Reset
+
+	# Error Report
+	'00 0a 00 01 ' + '00 00 00 11 ' + '00 00 00 00 ' + '00 00 00 00 ' + 'f0', # Internal Error with internal lengths less than total length
+	'00 0a 00 01 ' + '00 00 00 10 ' + '00 00 00 00 ' + '00 00 00 01', # Internal Error with internal lengths greater than total length
 ]
 
 bad_PDUs = [''.join([chr(int(c, 0x10)) for c in s.split(' ')]) for s in bad_PDUs_hex]
