1331f36f8182ddcd01ff27190e9514402f4ae1ff
David Mandelberg <dmandelb@bbn.com>
2011-11-29 20:11:21 -0500

    support generating all types of PDUs

--- rtr/test-client.c
+++ rtr/test-client.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #include "logutils.h"
 
@@ -17,9 +18,91 @@
 #define MAX_PDU_SIZE 65536
 #define LINEBUF_SIZE 128
 
+// all the arguments a command (for sending) can take
+enum command_argument {
+	ARG_NONCE,
+	ARG_SERIAL,
+	ARG_PREFIX_FLAGS,
+	ARG_PREFIX_LENGTH,
+	ARG_PREFIX_MAX_LENGTH,
+	ARG_IPv4,
+	ARG_IPv6,
+	ARG_AS_NUMBER,
+	ARG_ERROR_CODE,
+	ARG_END // special argument to indicate the end of the argument list
+};
+
+static const char * arg_name(enum command_argument arg)
+{
+	switch (arg)
+	{
+		case ARG_NONCE: return "nonce";
+		case ARG_SERIAL: return "serial";
+		case ARG_PREFIX_FLAGS: return "prefix flags";
+		case ARG_PREFIX_LENGTH: return "prefix length";
+		case ARG_PREFIX_MAX_LENGTH: return "prefix max length";
+		case ARG_IPv4: return "IPv4 address";
+		case ARG_IPv6: return "IPv6 address";
+		case ARG_AS_NUMBER: return "AS number";
+		case ARG_ERROR_CODE: return "error code";
+		default: return NULL;
+	}
+}
+
+
+struct command;
+
+static void cmd_serial_notify(const struct command * command, char const * const * args);
+static void cmd_serial_query(const struct command * command, char const * const * args);
+static void cmd_reset_query(const struct command * command, char const * const * args);
+static void cmd_cache_response(const struct command * command, char const * const * args);
+static void cmd_ipv4_prefix(const struct command * command, char const * const * args);
+static void cmd_ipv6_prefix(const struct command * command, char const * const * args);
+static void cmd_end_of_data(const struct command * command, char const * const * args);
+static void cmd_cache_reset(const struct command * command, char const * const * args);
+static void cmd_error_report(const struct command * command, char const * const * args);
+
+#define MAX_NUM_ARGS 10
+struct command {
+	char * name;
+	void (*function)(const struct command * command, char const * const * args);
+	enum command_argument args[MAX_NUM_ARGS];
+};
+
+static const struct command commands[] = {
+	{"serial_notify", cmd_serial_notify, {ARG_NONCE, ARG_SERIAL, ARG_END}},
+	{"serial_query", cmd_serial_query, {ARG_NONCE, ARG_SERIAL, ARG_END}},
+	{"reset_query", cmd_reset_query, {ARG_END}},
+	{"cache_response", cmd_cache_response, {ARG_NONCE, ARG_END}},
+	{"ipv4_prefix", cmd_ipv4_prefix, {ARG_PREFIX_FLAGS, ARG_PREFIX_LENGTH, ARG_PREFIX_MAX_LENGTH, ARG_IPv4, ARG_AS_NUMBER, ARG_END}},
+	{"ipv6_prefix", cmd_ipv6_prefix, {ARG_PREFIX_FLAGS, ARG_PREFIX_LENGTH, ARG_PREFIX_MAX_LENGTH, ARG_IPv6, ARG_AS_NUMBER, ARG_END}},
+	{"end_of_data", cmd_end_of_data, {ARG_NONCE, ARG_SERIAL, ARG_END}},
+	{"cache_reset", cmd_cache_reset, {ARG_END}},
+	{"error_report", cmd_error_report, {ARG_ERROR_CODE, ARG_END}},
+	{NULL, NULL, {ARG_END}}
+};
+
+static void command_print_usage_signature(
+	FILE * out,
+	const struct command * command,
+	const char * print_before,
+	const char * print_after)
+{
+	size_t i;
+
+	fprintf(out, "%s%s", print_before, command->name);
+	for (i = 0; command->args[i] != ARG_END; ++i)
+	{
+		fprintf(out, " <%s>", arg_name(command->args[i]));
+	}
+	fprintf(out, "%s", print_after);
+}
+
 
 static void do_help(const char * argv0)
 {
+	size_t i;
+
 	fprintf(stderr, "Usage:\n");
 	fprintf(stderr, "    %s [-h | --help]    Print this help text.\n", argv0);
 	fprintf(stderr, "    %s send             Run the sending end of a connection.\n", argv0);
@@ -28,20 +111,97 @@ static void do_help(const char * argv0)
 	fprintf(stderr, "    $ %s send | nc <rtr server host> <rtr server port> | %s recv\n", argv0, argv0);
 	fprintf(stderr, "\n");
 	fprintf(stderr, "On the sending end, you can type the following commands to send PDUs to the server:\n");
-	fprintf(stderr, "    serial <nonce> <serial>\n");
-	fprintf(stderr, "    reset\n");
-	fprintf(stderr, "    error <code>\n");
+	for (i = 0; commands[i].name != NULL; ++i)
+	{
+		command_print_usage_signature(stderr, &commands[i], "    ", "\n");
+	}
+}
+
+static inline bool _command_get_arg_sscanf(
+	const char * arg_string,
+	const char * format,
+	void * arg_value)
+{
+	return sscanf(arg_string, format, arg_value) == 1;
+}
+
+static bool command_get_arg(
+	const struct command * command,
+	char const * const * args,
+	size_t arg_index,
+	void * arg_value)
+{
+	char const * const arg_string = args[arg_index];
+	bool ret;
+
+	if (arg_string == NULL)
+	{
+		ret = false;
+		goto done;
+	}
+
+	switch(command->args[arg_index])
+	{
+		case ARG_NONCE:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu16, arg_value);
+			goto done;
+
+		case ARG_SERIAL:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu32, arg_value);
+			goto done;
+
+		case ARG_PREFIX_FLAGS:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu8, arg_value);
+			goto done;
+
+		case ARG_PREFIX_LENGTH:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu8, arg_value);
+			goto done;
+
+		case ARG_PREFIX_MAX_LENGTH:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu8, arg_value);
+			goto done;
+
+		case ARG_IPv4:
+			ret = inet_pton(AF_INET, arg_string, arg_value) == 1;
+			goto done;
+
+		case ARG_IPv6:
+			ret = inet_pton(AF_INET6, arg_string, arg_value) == 1;
+			goto done;
+
+		case ARG_AS_NUMBER:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu32, arg_value);
+			goto done;
+
+		case ARG_ERROR_CODE:
+			ret = _command_get_arg_sscanf(arg_string, "%" SCNu16, arg_value);
+			goto done;
+
+		default:
+			ret = false;
+			goto done;
+	}
+
+done:
+	if (!ret)
+	{
+		command_print_usage_signature(stderr, command, "usage: ", "\n");
+	}
+
+	return ret;
 }
 
 
-static bool send_pdu(const PDU * pdu, uint8_t buffer[MAX_PDU_SIZE])
+static void send_pdu(const PDU * pdu)
 {
+	uint8_t buffer[MAX_PDU_SIZE];
 	ssize_t length = dump_pdu(buffer, MAX_PDU_SIZE, pdu);
 
 	if (length < 0)
 	{
 		fprintf(stderr, "error in pdu to send");
-		return false;
+		exit(EXIT_FAILURE);
 	}
 
 	ssize_t retval;
@@ -53,24 +213,151 @@ static bool send_pdu(const PDU * pdu, uint8_t buffer[MAX_PDU_SIZE])
 		if (retval < 0)
 		{
 			perror("write()");
-			return false;
+			exit(EXIT_FAILURE);
 		}
 
 		offset += retval;
 	}
+}
 
-	return true;
+static void cmd_serial_notify(const struct command * command, char const * const * args)
+{
+	cache_nonce_t nonce;
+	serial_number_t serial;
+
+	if (!command_get_arg(command, args, 0, &nonce)) return;
+	if (!command_get_arg(command, args, 1, &serial)) return;
+
+	PDU pdu;
+	fill_pdu_serial_notify(&pdu, nonce, serial);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_serial_query(const struct command * command, char const * const * args)
+{
+	cache_nonce_t nonce;
+	serial_number_t serial;
+
+	if (!command_get_arg(command, args, 0, &nonce)) return;
+	if (!command_get_arg(command, args, 1, &serial)) return;
+
+	PDU pdu;
+	fill_pdu_serial_query(&pdu, nonce, serial);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_reset_query(const struct command * command, char const * const * args)
+{
+	(void)command;
+	(void)args;
+
+	PDU pdu;
+	fill_pdu_reset_query(&pdu);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_cache_response(const struct command * command, char const * const * args)
+{
+	cache_nonce_t nonce;
+
+	if (!command_get_arg(command, args, 0, &nonce)) return;
+
+	PDU pdu;
+	fill_pdu_cache_response(&pdu, nonce);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_ipv4_prefix(const struct command * command, char const * const * args)
+{
+	uint8_t flags;
+	uint8_t prefix_length;
+	uint8_t max_length;
+	struct in_addr prefix;
+	as_number_t asn;
+
+	if (!command_get_arg(command, args, 0, &flags)) return;
+	if (!command_get_arg(command, args, 1, &prefix_length)) return;
+	if (!command_get_arg(command, args, 2, &max_length)) return;
+	if (!command_get_arg(command, args, 3, &prefix)) return;
+	if (!command_get_arg(command, args, 4, &asn)) return;
+
+	PDU pdu;
+	fill_pdu_ipv4_prefix(&pdu, flags, prefix_length, max_length, &prefix, asn);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_ipv6_prefix(const struct command * command, char const * const * args)
+{
+	uint8_t flags;
+	uint8_t prefix_length;
+	uint8_t max_length;
+	struct in6_addr prefix;
+	as_number_t asn;
+
+	if (!command_get_arg(command, args, 0, &flags)) return;
+	if (!command_get_arg(command, args, 1, &prefix_length)) return;
+	if (!command_get_arg(command, args, 2, &max_length)) return;
+	if (!command_get_arg(command, args, 3, &prefix)) return;
+	if (!command_get_arg(command, args, 4, &asn)) return;
+
+	PDU pdu;
+	fill_pdu_ipv6_prefix(&pdu, flags, prefix_length, max_length, &prefix, asn);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_end_of_data(const struct command * command, char const * const * args)
+{
+	cache_nonce_t nonce;
+	serial_number_t serial;
+
+	if (!command_get_arg(command, args, 0, &nonce)) return;
+	if (!command_get_arg(command, args, 1, &serial)) return;
+
+	PDU pdu;
+	fill_pdu_end_of_data(&pdu, nonce, serial);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_cache_reset(const struct command * command, char const * const * args)
+{
+	(void)command;
+	(void)args;
+
+	PDU pdu;
+	fill_pdu_cache_reset(&pdu);
+
+	send_pdu(&pdu);
+}
+
+static void cmd_error_report(const struct command * command, char const * const * args)
+{
+	PDU pdu;
+
+	pdu.protocolVersion = RTR_PROTOCOL_VERSION;
+	if (!command_get_arg(command, args, 0, &pdu.errorCode)) return;
+	pdu.pduType = PDU_ERROR_REPORT;
+	pdu.length = PDU_HEADER_LENGTH + PDU_ERROR_HEADERS_LENGTH;
+	pdu.errorData.encapsulatedPDULength = 0;
+	pdu.errorData.encapsulatedPDU = NULL;
+	pdu.errorData.errorTextLength = 0;
+	pdu.errorData.errorText = NULL;
+
+	send_pdu(&pdu);
 }
 
 static int do_send()
 {
 	char linebuf[LINEBUF_SIZE];
 	char * tok;
-
-	PDU pdu;
-	uint8_t pdu_buffer[MAX_PDU_SIZE];
-
-	pdu.protocolVersion = RTR_PROTOCOL_VERSION;
+	char const * command_args[MAX_NUM_ARGS];
+	size_t i, j;
 
 	while (true)
 	{
@@ -85,61 +372,26 @@ static int do_send()
 		{
 			continue;
 		}
-		else if (strcmp(tok, "serial") == 0)
+
+		for (i = 0; commands[i].name != NULL; ++i)
 		{
-			tok = strtok(NULL, DELIM);
-			if (tok == NULL || sscanf(tok, "%" SCNu16, &pdu.cacheNonce) != 1)
+			if (strcmp(tok, commands[i].name) == 0)
 			{
-				fprintf(stderr, "usage: serial <nonce> <serial>\n");
-				continue;
+				for (j = 0; commands[i].args[j] != ARG_END; ++j)
+				{
+					command_args[j] = strtok(NULL, DELIM);
+				}
+
+				commands[i].function(&commands[i], command_args);
+				break;
 			}
-
-			tok = strtok(NULL, DELIM);
-			if (tok == NULL || sscanf(tok, "%" SCNu32, &pdu.serialNumber) != 1)
-			{
-				fprintf(stderr, "usage: serial <nonce> <serial>\n");
-				continue;
-			}
-
-			pdu.pduType = PDU_SERIAL_QUERY;
-			pdu.length = PDU_HEADER_LENGTH + sizeof(pdu.serialNumber);
-
-			if (!send_pdu(&pdu, pdu_buffer))
-				return EXIT_FAILURE;
 		}
-		else if (strcmp(tok, "reset") == 0)
+		if (commands[i].name != NULL)
 		{
-			pdu.pduType = PDU_RESET_QUERY;
-			pdu.reserved = 0;
-			pdu.length = PDU_HEADER_LENGTH;
-
-			if (!send_pdu(&pdu, pdu_buffer))
-				return EXIT_FAILURE;
-		}
-		else if (strcmp(tok, "error") == 0)
-		{
-			tok = strtok(NULL, DELIM);
-			if (tok == NULL || sscanf(tok, "%" SCNu16, &pdu.errorCode) != 1)
-			{
-				fprintf(stderr, "usage: error <code>\n");
-				continue;
-			}
-
-			pdu.pduType = PDU_ERROR_REPORT;
-			pdu.length = PDU_HEADER_LENGTH + PDU_ERROR_HEADERS_LENGTH;
-			pdu.errorData.encapsulatedPDULength = 0;
-			pdu.errorData.encapsulatedPDU = NULL;
-			pdu.errorData.errorTextLength = 0;
-			pdu.errorData.errorText = NULL;
-
-			if (!send_pdu(&pdu, pdu_buffer))
-				return EXIT_FAILURE;
-		}
-		else
-		{
-			fprintf(stderr, "invalid command: %s\n", tok);
 			continue;
 		}
+
+		fprintf(stderr, "invalid command: %s\n", tok);
 	}
 
 	return EXIT_FAILURE;
