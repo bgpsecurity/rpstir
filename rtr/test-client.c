b4c0a59cfd1e92493044c9fcb14ef1713e5086ad
David Mandelberg <dmandelb@bbn.com>
2011-12-12 14:16:41 -0500

    header changes for OpenBSD

--- rtr/test-client.c
+++ rtr/test-client.c
@@ -4,10 +4,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/time.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
 
