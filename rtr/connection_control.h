6cd793e638ff572cc9b3a8a8d196706981c649e8
David Mandelberg <dmandelb@bbn.com>
2011-12-13 11:31:04 -0500

    add a missing const

--- rtr/connection_control.h
+++ rtr/connection_control.h
@@ -5,7 +5,7 @@
 
 // memory is handled entirely by main
 struct connection_control_main_args {
-	int * listen_fds;
+	const int * listen_fds;
 	size_t num_listen_fds;
 	Queue * db_request_queue;
 	db_semaphore_t * db_semaphore;
