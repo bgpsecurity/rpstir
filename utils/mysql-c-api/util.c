c8efcd4d71522a4eeea4af4acc63ce7b4e711d7e
David Mandelberg <dmandelb@bbn.com>
2011-12-19 16:21:25 -0500

    don't reimplement memcpy()

--- utils/mysql-c-api/util.c
+++ utils/mysql-c-api/util.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <my_global.h>
 #include <mysql.h>
@@ -117,10 +118,8 @@ int getStringByFieldname(char **out, MYSQL_RES *result, MYSQL_ROW row, char fiel
         return -1;
     }
 
+    memcpy(*out, row[field_no], len);
     (*out)[len] = '\0';
-    for (i = 0; i < len; i++) {
-        (*out)[i] = row[field_no][i];
-    }
 
     return 0;
 }
