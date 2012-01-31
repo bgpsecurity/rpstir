429f28ab3719322131735c450cfb07ea0e8d28fc
David Mandelberg <dmandelb@bbn.com>
2012-01-31 14:22:19 -0500

    update test script

--- testcases/conformance/scripts/run_tests.sh
+++ testcases/conformance/scripts/run_tests.sh
@@ -10,30 +10,69 @@ cd "$THIS_SCRIPT_DIR"/..
 
 ./scripts/gen_all.sh
 
+cd output
+
+add_file () {
+	TYPE="$1" # "good" or "bad"
+	FLAGS="$2" # -f or -F
+	FILE="$3" # file to add
+
+	if test x"$TYPE" = "bad"; then
+		if "$RPKI_ROOT/proto/rcli" -y $FLAGS "$FILE"; then
+			echo >&2 "Error: adding bad file $FILE succeeded"
+			exit 1
+		fi
+	else
+		if ! "$RPKI_ROOT/proto/rcli" -y $FLAGS "$FILE"; then
+			echo >&2 "Error: adding good file $FILE failed"
+			exit 1
+		fi
+	fi
+}
+
 init_db () {
 	"$RPKI_ROOT/run_scripts/initDB.sh"
-	"$RPKI_ROOT/proto/rcli" -y -F raw/root.cer
 }
 
 reset_db () {
 	"$RPKI_ROOT/proto/rcli" -x -t "$RPKI_ROOT/REPOSITORY" -y
-	"$RPKI_ROOT/proto/rcli" -y -F raw/root.cer
+	add_file good -F root.cer
+	add_file good -f root/root.crl
+	add_file good -f root/root.mft
 }
+
 
 init_db
 
-for BAD_FILE in raw/root/bad*; do
+for BAD_ROOT in badRoot*.cer; do
 	reset_db
-	if "$RPKI_ROOT/proto/rcli" -y -f "$BAD_FILE"; then
-		echo >&2 "Error: adding bad file $BAD_FILE succeeded"
-		exit 1
-	fi
+	add_file bad -F "$BAD_ROOT"
 done
 
-for GOOD_FILE in raw/root/good*; do
+cd root
+
+for GOOD_SINGLE_FILE in good*; do
 	reset_db
-	if ! "$RPKI_ROOT/proto/rcli" -y -f "$GOOD_FILE"; then
-		echo >&2 "Error: adding good file $GOOD_FILE failed"
-		exit 1
-	fi
+	add_file good -f "$GOOD_SINGLE_FILE"
+done
+
+for BAD_SINGLE_FILE in bad*; do
+	reset_db
+	add_file bad -f "$BAD_SINGLE_FILE"
+done
+
+for CRL_CERT in CRL*.cer; do
+	CRL_NAME=`basename "$CRL_CERT" .cer`
+	reset_db
+	add_file good -f "$CRL_CERT"
+	add_file good -f "$CRL_NAME/$CRL_NAME.mft"
+	add_file bad -f "$CRL_NAME/bad$CRL_NAME.crl"
+done
+
+for MFT_CERT in MFT*.cer; do
+	MFT_NAME=`basename "$MFT_CERT" .cer`
+	reset_db
+	add_file good -f "$MFT_CERT"
+	add_file good -f "$MFT_NAME/$MFT_NAME.crl"
+	add_file bad -f "$MFT_NAME/bad$MFT_NAME.mft"
 done
