bab4256366ad04d0887ff24506b787db47167040
David Mandelberg <dmandelb@bbn.com>
2012-02-01 18:25:56 -0500

    fill in a bit more

--- run_scripts/chaser.sh
+++ run_scripts/chaser.sh
@@ -4,11 +4,11 @@
 #   used for doing the rsync_pull
 
 # set environment variables if not set
-THIS_SCRIPT_DIR=$(dirname $0)
+THIS_SCRIPT_DIR=`dirname "$0"`
 . $THIS_SCRIPT_DIR/../envir.setup
 
-# XXX: check this command line, also make sure it outputs NULL-separated URIs only, in sorted order
-CHASER="$RPKI_ROOT/proto/chaser ..."
+CHASER="$RPKI_ROOT/proto/chaser"
+RSYNC_CORD_CONF="$RPKI_ROOT/rsync_cord.config"
 
 OLD_LIST="`mktemp`"
 CUR_LIST="`mktemp`"
@@ -16,7 +16,9 @@ CUR_LIST="`mktemp`"
 $CHASER "$@" > "$CUR_LIST"
 
 while ! cmp -s "$OLD_LIST" "$CUR_LIST"; do
-	rsync, aur, rcli, etc "$CUR_LIST" # XXX
+	rm -f "$RSYNC_CORD_CONF"
+	# TODO: fill in $RSYNC_CORD_CONF from $CUR_LIST
+	python "$RPKI_ROOT"/rsync_aur/rsync_cord.py -d -c "$RSYNC_CORD_CONF" -t "$RPKI_TCOUNT" -p "$RPKI_LISTPORT"
 
 	rm -f "$OLD_LIST"
 	OLD_LIST="$CUR_LIST"
