5884ab5af32e61841ea2eb17159c054e0232a5a7
Andrew Chi <achi@bbn.com>
2012-02-12 22:09:48 -0500

    Fix rcli's socket error checking, enable SO_REUSEADDR for fast restart
    1. Fix sock1line: it previously would not detect a closed connection
       and would instead cause the CPU to be pegged (it would return NULL
       and then be called again immediately -- infinite loop).
    
    2. Limit makesock() retries to 1 per second; limit number of retries to 10.
    
    3. Enable SO_REUSEADDR (and put in the right place) so that a restart
       of rcli does not get caught with "address in use", even if the
       previous socket is still in TIME_WAIT.  This is safe because we
       expect connections to be local, and packet loss is highly unlikely.
    
    4. Have subsystemTest ensure that the listening socket is active
       before proceeding to invoke the steps that would send it data.

--- proto/rcli.c
+++ proto/rcli.c
@@ -261,7 +261,7 @@ static int makesock(char *porto, int *protosp)
   int  consumed;
   static int64_t num_accepted_connections = 0;
   static int64_t num_failed_connections = 0;
-//  int  one = 1;
+  int  one = 1;
   int  s;
 
   if ( sscanf(porto, "%" SCNu16 "%n", &port, &consumed) < 1 ||
@@ -272,13 +272,22 @@ static int makesock(char *porto, int *protosp)
   protos = *protosp;
   if ( protos < 0 )
     {
-      log_msg(LOG_INFO, "Creating a socket on port %s", porto);
+      log_msg(LOG_INFO, "Creating a socket on port %" PRIu16, port);
       protos = socket(AF_INET, SOCK_STREAM, 0);
       if ( protos < 0 )
         {
           perror("Failed to create socket");
           return(protos);
         }
+
+      // Enable address reuse, so that TIME_WAIT doesn't prevent restart.
+      sta = setsockopt(protos, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+      if (sta < 0)
+        {
+          log_msg(LOG_WARNING, "Failed to set SO_REUSEADDR");
+          perror("Failed to set SO_REUSEADDR");
+        }
+
       memset(&sinn, 0, sizeof(sinn));
       sinn.sin_addr.s_addr = htonl(INADDR_ANY);
       sinn.sin_family = AF_INET;
@@ -287,10 +296,9 @@ static int makesock(char *porto, int *protosp)
       if ( sta < 0 )
 	{
 	  perror("Failed to bind to port");
-	  close(protos);
+          close(protos);
 	  return(sta);
 	}
-//  (void)setsockopt(protos, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
       sta = listen(protos, 5);
       if ( sta < 0 )
 	{
@@ -415,11 +423,11 @@ static char *hasoneline(char *inp, char **nextp)
   case it returns a pointer to the first such line, and modifies left
   so that that line is deleted. If left does not contain a complete
   line this function will read as much socket data as it can. It will
-  stuff the first complete line (if any) into the return code, and
+  stuff the first complete line (if any) into 'line', and
   put the remaining stuff into left.
 */
 
-static char *sock1line(int s, char **leftp)
+static int sock1line(int s, char **leftp, char **line)
 {
   char *left2;
   char *left = *leftp;
@@ -433,24 +441,26 @@ static char *sock1line(int s, char **leftp)
   if ( ptr != NULL )  // left had at least one line
     {
       *leftp = next;
-      return(ptr);
+      *line = ptr;
+      return 0;
     }
   if ( left != NULL )
     leen = strlen(left);
   sta = ioctl(s, FIONREAD, &rd);
   if ( sta < 0 )
-    return(NULL);
+    return sta;
   /* Blocking mode, by A. Chi, 3/18/11.  Even if no data is available
      yet, block until we can read at least one byte. */
   if ( rd <= 0)
     rd = 1;
   left2 = (char *)calloc(leen+rd+1, sizeof(char));
-  if ( left2 == NULL )
-    return(NULL);
+  if ( left2 == NULL ) {
+    return -1;
+  }
   (void)strncpy(left2, left, leen);
-  sta = recv(s, left2+leen, rd, 0);
-  if ( sta < 0 )
-    sta = 0;
+  sta = recv(s, left2+leen, rd, MSG_WAITALL);
+  if (sta <= 0) // 0 indicates orderly connection shutdown
+    return -1;
   left2[leen+sta] = 0;
   //  free((void *)left);
   left = left2;
@@ -459,11 +469,13 @@ static char *sock1line(int s, char **leftp)
     {
       free((void *)*leftp);
       *leftp = next;
-      return(ptr);
+      *line = ptr;
+      return 0;
     }
   free((void *)*leftp);
   *leftp = left;
-  return(NULL);
+  *line = NULL;
+  return 0;
 }
 
 /*
@@ -582,7 +594,9 @@ static int sockline(scm *scmp, scmcon *conp, int s)
 	  return(sta);
 	}
       */
-      ptr = sock1line(s, &left);
+      sta = sock1line(s, &left, &ptr);
+      if (sta != 0)
+        return sta;
       if ( ptr == NULL )
 	continue;
       log_msg(LOG_INFO, "Sockline: %s", ptr);
@@ -1167,15 +1181,30 @@ int main(int argc, char **argv)
   if ( (do_sockopts+do_fileopts) > 0 && porto != NULL && sta == 0 )
     {
       int protos = (-1);
+      const int max_makesock_attempts = 10;
+      int makesock_failures = 0;
       do
 	{
 	  if ( do_sockopts > 0 )
 	    {
 	      s = makesock(porto, &protos);
 	      if ( s < 0 )
-		log_msg(LOG_ERR, "Could not create socket");
+                {
+                  makesock_failures++;
+                  log_msg(LOG_ERR, "Failed to listen on port %s (failure #%d)",
+                          porto, makesock_failures);
+                  sleep(1);
+                  if (makesock_failures >= max_makesock_attempts) {
+                    log_msg(LOG_ERR,
+                            "%d failed attempts to create socket. Aborting.",
+                            max_makesock_attempts);
+                    sta = -1;
+                    break;
+                  }
+                }
 	      else
 		{
+                  makesock_failures = 0;
 		  log_flush();
 		  sta = sockline(scmp, realconp, s);
 		  log_msg(LOG_INFO, "Socket connection closed");
