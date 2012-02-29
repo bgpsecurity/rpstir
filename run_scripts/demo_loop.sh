0eff427cc33ee12439fab749835ba17aefa4d39b
amw <aweber@bbn.com>
2012-02-29 15:03:54 -0500

    add to demo_loop.sh

--- run_scripts/demo_loop.sh
+++ run_scripts/demo_loop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# set environment variables if not set
+THIS_SCRIPT_DIR=$(dirname $0)
+. $THIS_SCRIPT_DIR/../envir.setup
+
 cd $RPKI_ROOT
 
 # If loader is not running, start it
@@ -18,6 +22,8 @@ proto/chaser -f initial_rsync.config
 rtr/rpstir-rtr-update run_scripts/sampleQuerySpecs
 
 # If rtr-server is not running, run it
-if [ -z "$(pgrep -u $USER rpstir-rtrd.sh)" ]; then
+if [ -z "$(pgrep -u $USER rpstir-rtrd)" ]; then
     rtr/rpstir-rtrd &
 fi
+
+echo "ran at `date`" >> /home/demouser/rpki/run_scripts/demo_loop.log
