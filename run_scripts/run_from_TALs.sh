2d19837bebbe75298fa0dedd8bcd682d0c83c207
David Mandelberg <dmandelb@bbn.com>
2012-03-01 10:10:02 -0500

    don't automatically fetch TALs from unprotected channels

--- run_scripts/run_from_TALs.sh
+++ run_scripts/run_from_TALs.sh
@@ -3,12 +3,11 @@
 cd $RPKI_ROOT
 
 TAL_FILES=""
-TAL_URI_FILES=""
 
 print_usage () {
-	echo >&2 "Usage: $0 <TAL uri> | <TAL file> ..."
+	echo >&2 "Usage: $0 <TAL file> ..."
 	echo >&2
-	echo >&2 "Run RPSTIR on the specified TAL URIs or files."
+	echo >&2 "Run RPSTIR on the specified TAL files."
 }
 
 for TAL in "$@"; do
@@ -19,16 +18,13 @@ for TAL in "$@"; do
 		echo >&2 "Using TAL $TAL"
 		TAL_FILES="$TAL_FILES $TAL"
 	else
-		TAL_FILE="`mktemp --suffix=.tal`"
-		echo >&2 "Downloading $TAL to $TAL_FILE"
-		wget "$TAL" -O "$TAL_FILE"
-		echo >&2 "Using TAL $TAL_FILE from $TAL"
-		TAL_URI_FILES="$TAL_URI_FILES $TAL_FILE"
+		echo >&2 "Can't find TAL $TAL"
+		exit 1
 	fi
 done
 
-if test -z "$TAL_FILES$TAL_URI_FILES"; then
-	echo >&2 "No TAL files or URIs specified"
+if test -z "$TAL_FILES"; then
+	echo >&2 "No TAL files specified"
 	echo >&2
 	print_usage
 	exit 1
@@ -46,12 +42,10 @@ LOADER_PID=$!
 sleep 1
 
 # Out-of-band initialization of trust anchor
-./run_scripts/updateTA.py $TAL_FILES $TAL_URI_FILES
+./run_scripts/updateTA.py $TAL_FILES
 
 # Chase downward from trust anchor(s) using rsync
 ./run_scripts/chaser.sh
 
 kill "$LOADER_PID"
 wait "$LOADER_PID"
-
-rm -f "$TAL_URI_FILES"
