6334017e8c7263b59dd78c0f3fe34f5d0aea683b
amw <aweber@bbn.com>
2012-03-02 18:52:42 -0500

    script tweaks

--- run_scripts/rpstir_rtrd_restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is only intended for the git branch demo.
-
-if [ -n "$(pgrep -u $USER rpstir-rtrd)" ]; then
-    kill "$(pgrep -u $USER rpstir-rtrd)"
-fi
-
-$RPKI_ROOT/rtr/rtrd &
