0484ef8c1d775c9f72d6f5c6712e175115fe31e9
amw <aweber@bbn.com>
2012-03-02 17:08:34 -0500

    some .sh tweaks

--- run_scripts/demo_init.sh
+++ run_scripts/demo_init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd $RPKI_ROOT
 
@@ -6,18 +6,18 @@ cd $RPKI_ROOT
 rm -rf REPOSITORY LOGS chaser.log rcli.log rsync_aur.log query.log
 mkdir -p REPOSITORY
 mkdir -p LOGS
-run_scripts/initDB.sh
+./run_scripts/initDB.sh
 
 # Start validator component in separate window
-run_scripts/loader.sh &
+./run_scripts/loader.sh &
 sleep 1
 
 # Out-of-band initialization of trust anchor
-#run_scripts/updateTA.py trust-anchor/afrinic.tal
-run_scripts/updateTA.py trust-anchor/apnic.tal
-#run_scripts/updateTA.py trust-anchor/arin.tal
-run_scripts/updateTA.py trust-anchor/lacnic.tal
-run_scripts/updateTA.py trust-anchor/ripe-ncc-root.tal
+#./run_scripts/updateTA.py trust-anchor/afrinic.tal
+./run_scripts/updateTA.py trust-anchor/apnic.tal
+#./run_scripts/updateTA.py trust-anchor/arin.tal
+./run_scripts/updateTA.py trust-anchor/lacnic.tal
+./run_scripts/updateTA.py trust-anchor/ripe-ncc-root.tal
 
 # Chase downward from trust anchor(s) using rsync
-proto/chaser -f initial_rsync.config
+./proto/chaser -f initial_rsync.config
--- run_scripts/demo_loop.sh
+++ run_scripts/demo_loop.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # set environment variables if not set
 THIS_SCRIPT_DIR=$(dirname $0)
@@ -8,22 +8,22 @@ cd $RPKI_ROOT
 
 # If loader is not running, start it
 if [ -z "$(pgrep -u $USER loader.sh)" ]; then
-    run_scripts/loader.sh &
+    ./run_scripts/loader.sh &
     sleep 1
 fi
 
-# Run the garbage collector
-run_scripts/garbage.sh
-
 # Run the rsync URI chaser
-proto/chaser -f initial_rsync.config
+./proto/chaser -f initial_rsync.config
+
+# Run the garbage collector
+./run_scripts/garbage.sh
 
 # Update the data for the rtr-server
-rtr/rpstir-rtr-update run_scripts/sampleQuerySpecs
+./rtr/rpstir-rtr-update run_scripts/sampleQuerySpecs
 
 # If rtr-server is not running, run it
 if [ -z "$(pgrep -u $USER rpstir-rtrd)" ]; then
-    rtr/rpstir-rtrd &
+    ./rtr/rpstir-rtrd &
 fi
 
-echo "ran at `date`" >> /home/demouser/rpki/run_scripts/demo_loop.log
+echo "---------- ran at `date` ----------"
