57109e5102e39b3a111b73c98d87a9a8eae49c05
David Mandelberg <dmandelb@bbn.com>
2012-06-04 13:32:53 -0400

    start filling in config_parse_file()

--- config/config_parse.c
+++ config/config_parse.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "logging.h"
+
 #include "config.h"
 #include "config_parse.h"
 
@@ -8,6 +13,46 @@ bool config_parse_file(
 	struct config_context * head,
 	struct config_context * tail)
 {
-	// TODO
-	return false;
+	char * line = NULL;
+	FILE * file = NULL;
+	bool ret = true;
+
+	line = malloc(MAX_LINE_LENGTH);
+	if (line == NULL)
+	{
+		LOG(LOG_ERROR, "out of memory");
+		ret = false;
+		goto done;
+	}
+
+	file = fopen(tail->file, "r");
+	if (file == NULL)
+	{
+		config_message(head, LOG_ERROR, "cannot open config file %s: %s",
+			tail->file, strerror(errno));
+		ret = false;
+		goto done;
+	}
+
+	// TODO: parse the file
+
+done:
+
+	if (file != NULL)
+	{
+		if (fclose(file) != 0)
+		{
+			config_message(head, LOG_ERROR, "cannot close config file %s: %s",
+				tail->file, strerror(errno));
+		}
+		file = NULL;
+	}
+
+	if (line != NULL)
+	{
+		free(line);
+		line = NULL;
+	}
+
+	return ret;
 }
