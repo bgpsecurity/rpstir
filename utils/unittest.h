6c613a6aa7a8624b2a871db590be6b6675307997
David Mandelberg <dmandelb@bbn.com>
2012-06-05 16:36:18 -0400

    add macro TEST_STR to test two strings by value

--- utils/unittest.h
+++ utils/unittest.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 // Call like TEST(int, "%d", 1 + 1, ==, 2) to ensure that 1+1 == 2
 #define TEST(type, fmt, value, condition, cmp_value) \
@@ -34,6 +35,26 @@
 			fprintf(stderr, "    Expected to be %s: %s\n", \
 				(_TEST_ ## __LINE__ ## _cmp_value ? "true" : "false"), \
 				#value); \
+			return false; \
+		} \
+	} while (false)
+
+// Call like TEST_STR("foo", !=, "bar")
+#define TEST_STR(value, condition, cmp_value) \
+	do { \
+		const char * _TEST_ ## __LINE__ ## _value = (value); \
+		const char *  _TEST_ ## __LINE__ ## _cmp_value = (cmp_value); \
+		if (!(strcmp(_TEST_ ## __LINE__ ## _value, _TEST_ ## __LINE__ ## _cmp_value) \
+			condition 0)) \
+		{ \
+			fprintf(stderr, "Failed test in %s:%d.\n", __FILE__, __LINE__); \
+			fprintf(stderr, "    Expected: %s %s %s\n", #value, #condition, #cmp_value); \
+			fprintf(stderr, "    Got LHS: %s = \"%s\"\n", \
+				#value, \
+				_TEST_ ## __LINE__ ## _value); \
+			fprintf(stderr, "    Got RHS: %s = \"%s\"\n", \
+				#cmp_value, \
+				_TEST_ ## __LINE__ ## _cmp_value); \
 			return false; \
 		} \
 	} while (false)
