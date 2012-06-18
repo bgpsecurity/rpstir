db9361078fd8ad8c55d76d0a7c1ae046776eee2c
David Mandelberg <dmandelb@bbn.com>
2012-06-18 19:19:34 -0400

    move oidtable

---
+++ mk/oidtable.mk
@@ -0,0 +1,19 @@
+pkgdata_DATA += var/oidtable
+CLEANFILES += var/oidtable
+
+var_oidtable_ASN1_SOURCES = \
+	lib/rpki-asn1/Algorithms.asn \
+	lib/rpki-asn1/blob.asn \
+	lib/rpki-asn1/certificate.asn \
+	lib/rpki-asn1/crlv2.asn \
+	lib/rpki-asn1/extensions.asn \
+	lib/rpki-asn1/keyfile.asn \
+	lib/rpki-asn1/manifest.asn \
+	lib/rpki-asn1/name.asn \
+	lib/rpki-asn1/orname.asn \
+	lib/rpki-asn1/privkey.asn \
+	lib/rpki-asn1/roa.asn \
+	lib/rpki-asn1/serial_number.asn
+
+var/oidtable: ./bin/asn1/make_oidtable $(var_oidtable_ASN1_SOURCES)
+	./bin/asn1/make_oidtable $(var_oidtable_ASN1_SOURCES) var/oidtable
