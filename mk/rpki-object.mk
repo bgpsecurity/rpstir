1f5a931c729d5f88db5861c489d987b01cf150e6
David Mandelberg <dmandelb@bbn.com>
2012-06-19 11:41:57 -0400

    add bin/rpki-object makefile

---
+++ mk/rpki-object.mk
@@ -0,0 +1,61 @@
+bin_PROGRAMS += bin/rpki-object/add_cms_cert
+
+
+bin_PROGRAMS += bin/rpki-object/add_cms_cert_no_check
+
+
+bin_PROGRAMS += bin/rpki-object/add_key_info
+
+
+bin_PROGRAMS += bin/rpki-object/check_cert_sig
+
+
+bin_PROGRAMS += bin/rpki-object/check_signature
+
+
+bin_PROGRAMS += bin/rpki-object/extractCMScert
+
+
+bin_PROGRAMS += bin/rpki-object/extractPubKeyInfo
+
+
+bin_PROGRAMS += bin/rpki-object/extractSIA
+
+
+bin_PROGRAMS += bin/rpki-object/extractValidityDate
+
+
+bin_PROGRAMS += bin/rpki-object/fix_manifest
+
+
+bin_PROGRAMS += bin/rpki-object/gen_key
+
+
+bin_PROGRAMS += bin/rpki-object/get_sernum
+
+
+bin_PROGRAMS += bin/rpki-object/loadkey
+
+
+bin_PROGRAMS += bin/rpki-object/put_sernum
+
+
+bin_PROGRAMS += bin/rpki-object/put_sia
+
+
+bin_PROGRAMS += bin/rpki-object/put_subj
+
+
+bin_PROGRAMS += bin/rpki-object/read_roa
+
+
+bin_PROGRAMS += bin/rpki-object/set_cert_ski
+
+
+bin_PROGRAMS += bin/rpki-object/sign_cert
+
+
+bin_PROGRAMS += bin/rpki-object/sign_cms
+
+
+bin_PROGRAMS += bin/rpki-object/update_cert
