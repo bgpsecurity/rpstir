1358e6fdbe03079060a4600064a00d9b3ca58e0d
David Mandelberg <dmandelb@bbn.com>
2012-06-19 14:02:07 -0400

    move utils/mysql-c-api

---
+++ mk/libdb.mk
@@ -0,0 +1,17 @@
+noinst_LIBRARIES += lib/db/libdb.a
+
+lib_db_libdb_a_SOURCES = \
+	lib/db/clients/chaser.c \
+	lib/db/clients/chaser.h \
+	lib/db/clients/rtr.c \
+	lib/db/clients/rtr.h \
+	lib/db/connect.c \
+	lib/db/connect.h \
+	lib/db/db-internal.h \
+	lib/db/prep-stmt.c \
+	lib/db/prep-stmt.h \
+	lib/db/util.c \
+	lib/db/util.h
+
+lib_db_libdb_a_CFLAGS = \
+	$(CFLAGS_STRICT)
