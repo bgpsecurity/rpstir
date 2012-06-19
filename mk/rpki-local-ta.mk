917f8aefd4bf0a5cb58c4f6177dd9cf97babd789
David Mandelberg <dmandelb@bbn.com>
2012-06-19 11:29:14 -0400

    makefile for bin/rpki-local-ta

---
+++ mk/rpki-local-ta.mk
@@ -0,0 +1,18 @@
+bin_PROGRAMS += bin/rpki-local-ta/dumpIPAddr
+
+bin_rpki_local_ta_dumpIPAddr_CFLAGS = \
+	-Wall \
+	-g
+
+bin_rpki_local_ta_dumpIPAddr_SOURCES = \
+	bin/rpki-local-ta/dumpIPAddr.c
+
+
+bin_PROGRAMS += bin/rpki-local-ta/test_cert
+
+bin_rpki_local_ta_test_cert_CFLAGS = \
+	-Wall \
+	-g
+
+bin_rpki_local_ta_test_cert_SOURCES = \
+	bin/rpki-local-ta/test_cert.c
