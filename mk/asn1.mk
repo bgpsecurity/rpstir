92af5bfb282c646ab8ad7df4917b220be3169303
David Mandelberg <dmandelb@bbn.com>
2012-06-19 11:45:06 -0400

    remove SOURCES that can be inferred by make

--- mk/asn1.mk
+++ mk/asn1.mk
@@ -14,23 +14,14 @@ bin_asn1_dump_LDADD = \
 
 bin_PROGRAMS += bin/asn1/dump_smart
 
-bin_asn1_dump_smart_SOURCES = \
-	dump_smart.c
-
 
 bin_PROGRAMS += bin/asn1/make_oidtable
-
-bin_asn1_make_oidtable_SOURCES = \
-	make_oidtable.c
 
 
 bin_PROGRAMS += bin/asn1/rr
 
 bin_asn1_rr_CPPFLAGS = \
 	-Ilib/casn
-
-bin_asn1_rr_SOURCES = \
-	rr.c
 
 bin_asn1_rr_LDADD = \
 	lib/casn/libcasn.a
--- mk/rpki-local-ta.mk
+++ mk/rpki-local-ta.mk
@@ -1,10 +1,4 @@
 bin_PROGRAMS += bin/rpki-local-ta/dumpIPAddr
 
-bin_rpki_local_ta_dumpIPAddr_SOURCES = \
-	bin/rpki-local-ta/dumpIPAddr.c
-
 
 bin_PROGRAMS += bin/rpki-local-ta/test_cert
-
-bin_rpki_local_ta_test_cert_SOURCES = \
-	bin/rpki-local-ta/test_cert.c
