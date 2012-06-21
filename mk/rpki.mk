d3c7b7f5a6bf9b94535386828d0af3191c94d42f
David Mandelberg <dmandelb@bbn.com>
2012-06-21 15:55:31 -0400

    move out run_scripts

--- mk/rpki.mk
+++ mk/rpki.mk
@@ -10,13 +10,28 @@ bin_rpki_garbage_LDADD = \
 	lib/rpki/librpki.a
 
 
+dist_bin_SCRIPTS += bin/rpki/garbage.sh
+
+
+dist_bin_SCRIPTS += bin/rpki/initDB.sh
+
+
+dist_bin_SCRIPTS += bin/rpki/loader.sh
+
+
 bin_PROGRAMS += bin/rpki/query
 
 bin_rpki_query_LDADD = \
 	lib/rpki/librpki.a
 
 
+dist_bin_SCRIPTS += bin/rpki/query.sh
+
+
 bin_PROGRAMS += bin/rpki/rcli
 
 bin_rpki_rcli_LDADD = \
 	lib/rpki/librpki.a
+
+
+dist_sysconf_DATA += etc/sampleQuerySpecs
