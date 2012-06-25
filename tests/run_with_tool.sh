8153e75e3a431bbacbb2fa152edfc0279288f96f
David Mandelberg <dmandelb@bbn.com>
2012-06-25 12:52:21 -0400

    run shell scripts normally

--- tests/run_with_tool.sh
+++ tests/run_with_tool.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -e
 
-TEST_LOG_NAME=check
-STRICT_CHECKS=1
-. `dirname "$0"`/../subsystemTests/test.include
-
 PROG=`basename "$1"`
 cd `dirname "$1"`
 shift
 
-run "$PROG" ./"$PROG" "$@" || exit $?
+case "$PROG" in
+    *.sh)
+        ./"$PROG" "$@" || exit $?
+        ;;
+
+    *)
+        TEST_LOG_NAME=check
+        STRICT_CHECKS=1
+        . `dirname "$0"`/../subsystemTests/test.include
+
+        run "$PROG" ./"$PROG" "$@" || exit $?
+        ;;
+esac
