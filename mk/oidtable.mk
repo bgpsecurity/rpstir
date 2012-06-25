0145a448abb7071a09723ce1e7f92efffbd22de6
David Mandelberg <dmandelb@bbn.com>
2012-06-25 19:08:51 -0400

    fix var/oidtable

--- mk/oidtable.mk
+++ mk/oidtable.mk
@@ -1,19 +1,19 @@
 pkgdata_DATA += var/oidtable
 CLEANFILES += var/oidtable
 
-var_oidtable_ASN1 = \
-	lib/rpki-asn1/Algorithms.asn \
-	lib/rpki-asn1/blob.asn \
-	lib/rpki-asn1/certificate.asn \
-	lib/rpki-asn1/crlv2.asn \
-	lib/rpki-asn1/extensions.asn \
-	lib/rpki-asn1/keyfile.asn \
-	lib/rpki-asn1/manifest.asn \
-	lib/rpki-asn1/name.asn \
-	lib/rpki-asn1/orname.asn \
-	lib/rpki-asn1/privkey.asn \
-	lib/rpki-asn1/roa.asn \
-	lib/rpki-asn1/serial_number.asn
+var_oidtable_ASN1_H = \
+	lib/rpki-asn1/Algorithms.h \
+	lib/rpki-asn1/blob.h \
+	lib/rpki-asn1/certificate.h \
+	lib/rpki-asn1/crlv2.h \
+	lib/rpki-asn1/extensions.h \
+	lib/rpki-asn1/keyfile.h \
+	lib/rpki-asn1/manifest.h \
+	lib/rpki-asn1/name.h \
+	lib/rpki-asn1/orname.h \
+	lib/rpki-asn1/privkey.h \
+	lib/rpki-asn1/roa.h \
+	lib/rpki-asn1/serial_number.h
 
-var/oidtable: ./bin/asn1/make_oidtable $(var_oidtable_ASN1)
-	./bin/asn1/make_oidtable $(var_oidtable_ASN1) var/oidtable
+var/oidtable: ./bin/asn1/make_oidtable $(var_oidtable_ASN1_H)
+	./bin/asn1/make_oidtable var/oidtable $(var_oidtable_ASN1_H)
