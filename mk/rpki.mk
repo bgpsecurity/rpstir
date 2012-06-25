c006e7f9a52024bb1007333019d044f69c9810b8
David Mandelberg <dmandelb@bbn.com>
2012-06-25 12:51:56 -0400

    switch chaser tests to new makefile

--- mk/rpki.mk
+++ mk/rpki.mk
@@ -3,6 +3,28 @@ bin_PROGRAMS += bin/rpki/chaser
 bin_rpki_chaser_LDADD = \
 	lib/db/libdb.a
 
+dist_check_SCRIPTS += tests/subsystem/chaser/test.sh
+
+TESTS += tests/subsystem/chaser/test.sh
+
+dist_check_DATA += \
+	tests/subsystem/chaser/input.bad_chars \
+	tests/subsystem/chaser/input.collapse_dots \
+	tests/subsystem/chaser/input.collapse_slash_dot \
+	tests/subsystem/chaser/input.collapse_slashes \
+	tests/subsystem/chaser/input.max_length \
+	tests/subsystem/chaser/input.subsume \
+	tests/subsystem/chaser/response.bad_chars.log.correct \
+	tests/subsystem/chaser/response.collapse_dots.log.correct \
+	tests/subsystem/chaser/response.collapse_slash_dot.log.correct \
+	tests/subsystem/chaser/response.collapse_slashes.log.correct \
+	tests/subsystem/chaser/response.max_length.log.correct \
+	tests/subsystem/chaser/response.subsume.log.correct
+
+CLEANFILES += \
+	tests/subsystem/chaser/*.diff \
+	tests/subsystem/chaser/*.log
+
 
 bin_PROGRAMS += bin/rpki/garbage
 
