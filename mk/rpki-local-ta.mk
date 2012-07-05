1405e5db2d55bc53c4fc7d0613afe54fb76eda3c
David Mandelberg <dmandelb@bbn.com>
2012-07-05 15:28:38 -0400

    add a file missing from the tarball

--- mk/rpki-local-ta.mk
+++ mk/rpki-local-ta.mk
@@ -35,6 +35,7 @@ tests_subsystem_rpki_local_ta_makeLTAtest_LDADD = \
 
 
 dist_check_DATA += \
+	tests/subsystem/rpki-local-ta/test4.log \
 	tests/subsystem/rpki-local-ta/testcases4_LTA \
 	tests/subsystem/rpki-local-ta/ttest*.par
 
