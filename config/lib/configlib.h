da6a03a3f610657fc1e5d348ba479917de08917a
David Mandelberg <dmandelb@bbn.com>
2012-08-27 10:57:07 -0400

    fix whitespace

--- config/lib/configlib.h
+++ config/lib/configlib.h
@@ -13,48 +13,44 @@ struct config_context;
 
 
 /**
-	Callback for config type functions to use to log messages about their config item.
+    Callback for config type functions to use to log messages about their config item.
 
-	@param context	Opaque data. Might include things like line number in the config file.
-	@param priority	See syslog(3).
+    @param context Opaque data. Might include things like line number in the config file.
+    @param priority See syslog(3).
 */
 void config_message(
     const struct config_context *context,
     int priority,
     const char *format,
-    ...) WARN_PRINTF(
-    3,
-    4);
+    ...)
+    WARN_PRINTF(3, 4);
 
 
 /**
-	Converts a string to a value of the correct type for the config item.
+    Converts a string to a value of the correct type for the config item.
 
-	@note Arrays have this called once for each value.
+    @note Arrays have this called once for each value.
 
-	@param[in] usr_arg	User argument provided to the callback.
-	@param[in] input	String from the config file. This may be copied,
-				but the pointer itself must not be put in *data.
-	@param[out] data	A value of the correct type for the config item.
-				May be NULL.
-	@return			True on success, false on failure. This means that
-				this function can be used to validate input.
+    @param[in] usr_arg User argument provided to the callback.
+    @param[in] input String from the config file. This may be copied,
+                     but the pointer itself must not be put in *data.
+    @param[out] data A value of the correct type for the config item.
+                     May be NULL.
+    @return True on success, false on failure. This means that
+            this function can be used to validate input.
 */
-     typedef bool(
-    *config_value_converter) (
+typedef bool (*config_value_converter) (
     const struct config_context * context,
     void *usr_arg,
     const char *input,
     void **data);
 
 /** Deep free data for a config item. */
-     typedef void (
-    *config_value_free) (
+typedef void (*config_value_free) (
     void *data);
 
 /** Check an array of values for inter-value consistency/correctness. */
-     typedef bool(
-    *config_array_validator) (
+typedef bool (*config_array_validator) (
     const struct config_context * context,
     void *usr_arg,
     void const *const *input,
@@ -63,61 +59,61 @@ void config_message(
 
 
 /** Structure to describe an available config option. */
-     struct config_option {
-         // configuration key, e.g. "SomeOption"
-         char *name;
+struct config_option {
+    // configuration key, e.g. "SomeOption"
+    char *name;
 
-         // type information
-         bool is_array;
-         config_value_converter value_convert;
-         void *value_convert_usr_arg;
-         config_value_free value_free;
-         config_array_validator array_validate;
-         void *array_validate_usr_arg;
+    // type information
+    bool is_array;
+    config_value_converter value_convert;
+    void *value_convert_usr_arg;
+    config_value_free value_free;
+    config_array_validator array_validate;
+    void *array_validate_usr_arg;
 
-         // Default value, as if it came from the config file.
-         // NULL indicates that the option is mandatory.
-         char *default_value;
-     };
+    // Default value, as if it came from the config file.
+    // NULL indicates that the option is mandatory.
+    char *default_value;
+};
 
 
 /** Return the value for a non-array config option. */
-     const void *config_get(
+const void *config_get(
     size_t key);
 
 /** Return the length of an array config option. */
-     size_t config_get_length(
+size_t config_get_length(
     size_t key);
 
 /** Return the values for an array config option. */
-     void const *const *config_get_array(
+void const * const * config_get_array(
     size_t key);
 
 /**
-	Load configuration data from a config file.
+    Load configuration data from a config file.
 
-	@note This MUST be called before any threads that could possibly
-		use configuration data are started.
+    @note This MUST be called before any threads that could possibly
+          use configuration data are started.
 
-	@param num_options	Number of config options.
-	@param options		Description of options.
-	@param filename	The file to load data from. If this is NULL, the
-			default configuration file is used.
+    @param num_options Number of config options.
+    @param options Description of options.
+    @param filename The file to load data from. If this is NULL, the
+                    default configuration file is used.
 */
-     bool config_load(
+bool config_load(
     size_t num_options,
     const struct config_option *options,
     const char *filename);
 
 /**
-	Call this after configuration data is no longer needed to free resources.
+    Call this after configuration data is no longer needed to free resources.
 
-	This is usually only called before a program exits.
+    This is usually only called before a program exits.
 
-	@note This MUST NOT be called when any threads could possibly use
-		configuration data.
+    @note This MUST NOT be called when any threads could possibly use
+          configuration data.
 */
-     void config_unload(
+void config_unload(
     );
 
 #endif
