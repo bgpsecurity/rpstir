d1e4b1999c1124b6e7719068ca892c000e570963
David Mandelberg <dmandelb@bbn.com>
2012-08-30 14:25:20 -0400

    update for new struct config_option

--- lib/config/config.c
+++ lib/config/config.c
@@ -17,6 +17,7 @@ static const struct config_option config_options[] = {
      "Root",
      false,
      config_type_string_converter, NULL,
+     NULL, NULL,
      free,
      NULL, NULL,
      "\"" ABS_TOP_SRCDIR "\""},
