f245a0359980bb23d091bc19ec09179721b34662
David Mandelberg <dmandelb@bbn.com>
2012-09-10 13:49:10 -0400

    collect more data

--- bin/rpki/collect-stats.sh
+++ bin/rpki/collect-stats.sh
@@ -21,6 +21,26 @@ STATS_DIR="statistics/$SYNC_START_TIME~$SYNC_END_TIME"
 mkdir "$STATS_DIR" || fatal "could not create $STATS_DIR"
 
 
+cp -R LOGS "$STATS_DIR" \
+    || fatal "could not copy LOGS"
+
+ls -lR REPOSITORY \
+    > "$STATS_DIR/REPOSITORY.list" \
+    || fatal "could not list REPOSITORY"
+
+
 query.sh -t cert -d pathname -d valfrom -d valto -d flags -i \
-    > "$STATS_DIR/query.cert" \
+    > "$STATS_DIR/query.cer" \
     || fatal "could not query certificates"
+
+query.sh -t crl -d pathname -d last_upd -d next_upd -d flags -i \
+    > "$STATS_DIR/query.crl" \
+    || fatal "could not query CRLs"
+
+query.sh -t roa -d pathname -d flags -i \
+    > "$STATS_DIR/query.roa" \
+    || fatal "could not query ROAs"
+
+query.sh -t manifest -d pathname -d this_upd -d next_upd -d flags -i \
+    > "$STATS_DIR/query.mft" \
+    || fatal "could not query manifests"
