8e14c52284dd9f2caf9943c13cd8425457cc9dd0
David Mandelberg <dmandelb@bbn.com>
2012-10-08 15:52:15 -0400

    remove more $CONFIG_ROOT_DIR

--- bin/rpki/initDB.sh
+++ bin/rpki/initDB.sh
@@ -5,8 +5,8 @@
 THIS_SCRIPT_DIR=$(dirname $0)
 . $THIS_SCRIPT_DIR/../../etc/envir.setup
 
-mkdir -p "$CONFIG_ROOT_DIR/REPOSITORY"
-mkdir -p "$CONFIG_ROOT_DIR/LOGS"
+mkdir -p "`config_get RPKICacheDir`"
+mkdir -p "`config_get LogDir`"
 
 echo About to clear database ...
-rcli -x -t "$CONFIG_ROOT_DIR/REPOSITORY" -y
+rcli -x -t "`config_get RPKICacheDir`" -y
