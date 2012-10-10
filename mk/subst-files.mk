b141ed18504955dd5e8d9bbfae7e501a823f0016
David Mandelberg <dmandelb@bbn.com>
2012-10-10 12:33:00 -0400

    simplify handling of .in files by make

--- mk/subst-files.mk
+++ mk/subst-files.mk
@@ -1,34 +1,11 @@
-do_subst = $(SED) \
-	-e 's,[@]abs_top_builddir[@],$(abs_top_builddir),g' \
-	-e 's,[@]abs_top_srcdir[@],$(abs_top_srcdir),g' \
-	-e 's,[@]pkgcachedir[@],$(pkgcachedir),g' \
-	-e 's,[@]pkglibexecdir[@],$(pkglibexecdir),g' \
-	-e 's,[@]pkglogdir[@],$(pkglogdir),g' \
-	-e 's,[@]CONFIG_ENV_VAR[@],$(CONFIG_ENV_VAR),g' \
-	-e 's,[@]MKTEMP[@],$(MKTEMP),g'
-
-
-# TODO: figure out where to distribute envir.setup
-BUILT_SOURCES += etc/envir.setup
 noinst_DATA += etc/envir.setup
-CLEANFILES += etc/envir.setup
-EXTRA_DIST += etc/envir.setup.in
-etc/envir.setup: $(top_srcdir)/etc/envir.setup.in Makefile
-	$(do_subst) < $(top_srcdir)/etc/envir.setup.in > "$@" || \
-		rm -f "$@"
+MK_SUBST_FILES += etc/envir.setup
+etc/envir.setup: $(srcdir)/etc/envir.setup.in
 
-BUILT_SOURCES += etc/rpstir.conf
 examples_DATA += etc/rpstir.conf
-CLEANFILES += etc/rpstir.conf
-EXTRA_DIST += etc/rpstir.conf.in
-etc/rpstir.conf: $(top_srcdir)/etc/rpstir.conf.in Makefile
-	$(do_subst) < $(top_srcdir)/etc/rpstir.conf.in > "$@" || \
-		rm -f "$@"
+MK_SUBST_FILES += etc/rpstir.conf
+etc/rpstir.conf: $(srcdir)/etc/rpstir.conf.in
 
-BUILT_SOURCES += tests/test.include
 noinst_DATA += tests/test.include
-CLEANFILES += tests/test.include
-EXTRA_DIST += tests/test.include.in
-tests/test.include: $(top_srcdir)/tests/test.include.in Makefile
-	$(do_subst) < $(top_srcdir)/tests/test.include.in > "$@" || \
-		rm -f "$@"
+MK_SUBST_FILES += tests/test.include
+tests/test.include: $(srcdir)/tests/test.include.in
