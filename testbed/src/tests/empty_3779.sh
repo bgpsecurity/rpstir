8c4fbaa59b2540a4be53f2acf892000b54a535c3
Andrew Chi <achi@bbn.com>
2012-10-23 16:28:55 -0400

    add test cases: create_object ipv4-only, ipv6-only, as-only

--- testbed/src/tests/empty_3779.sh
+++ testbed/src/tests/empty_3779.sh
@@ -13,6 +13,8 @@ cd "$RPKI_ROOT/testbed/src"
 GEN_KEY="$RPKI_ROOT/cg/tools/gen_key"
 CREATE_OBJECT="$RPKI_ROOT/testbed/src/create_object"
 
+# Intentionally violates RFC 6487 by having no RFC 3779 resources.  We
+# want to be able to produce this case.
 "$GEN_KEY" "$OUTDIR/root.p15" 2048
 "$CREATE_OBJECT" CERT \
 	outputfilename="$OUTDIR/root.cer" \
@@ -24,4 +26,46 @@ CREATE_OBJECT="$RPKI_ROOT/testbed/src/create_object"
 	subject="root" \
 	notbefore=120101010101Z \
 	notafter=490101010101Z \
-	sia="r:rsync://example.com/rpki/,m:rsync://example.com/rpki/empty_3779.mft"
+	sia="r:rsync://example.com/rpki/,m:rsync://example.com/empty_3779.mft"
+
+# Just IPv4
+"$CREATE_OBJECT" CERT \
+	outputfilename="$OUTDIR/root.cer" \
+	subjkeyfile="$OUTDIR/root.p15" \
+	type=CA \
+	selfsigned=true \
+	serial=1 \
+	issuer="root" \
+	subject="root" \
+	notbefore=120101010101Z \
+	notafter=490101010101Z \
+	sia="r:rsync://example.com/rpki/,m:rsync://example.com/empty_3779.mft" \
+        ipv4="0.0.0.0/0"
+
+# Just IPv6
+"$CREATE_OBJECT" CERT \
+	outputfilename="$OUTDIR/root.cer" \
+	subjkeyfile="$OUTDIR/root.p15" \
+	type=CA \
+	selfsigned=true \
+	serial=1 \
+	issuer="root" \
+	subject="root" \
+	notbefore=120101010101Z \
+	notafter=490101010101Z \
+	sia="r:rsync://example.com/rpki/,m:rsync://example.com/empty_3779.mft" \
+        ipv6="::/0"
+
+# Just AS numbers
+"$CREATE_OBJECT" CERT \
+	outputfilename="$OUTDIR/root.cer" \
+	subjkeyfile="$OUTDIR/root.p15" \
+	type=CA \
+	selfsigned=true \
+	serial=1 \
+	issuer="root" \
+	subject="root" \
+	notbefore=120101010101Z \
+	notafter=490101010101Z \
+	sia="r:rsync://example.com/rpki/,m:rsync://example.com/empty_3779.mft" \
+        as=0-4294967295
