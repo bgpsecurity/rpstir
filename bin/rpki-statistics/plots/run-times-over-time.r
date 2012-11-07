b8592591ea702b2f12ead53677bfe603d0bc5940
David Mandelberg <dmandelb@bbn.com>
2012-11-07 10:35:27 -0500

    fix up the plot a bit

--- bin/rpki-statistics/plots/run-times-over-time.r
+++ bin/rpki-statistics/plots/run-times-over-time.r
@@ -1,6 +1,9 @@
 data <- read.table('run-times-over-time.dat', sep="\t", header=TRUE)
 data$Start <- as.POSIXlt(read.table('times.dat', sep="\t", header=TRUE)$Start)
 
-png('run-times-over-time.png')
-plot(data)
+png('run-times-over-time.png', width=1600, height=1200, pointsize=24)
+plot(data$Start, data$Duration/60, type="o",
+    main="Combined Fetch and Validation Times",
+    xlab="Start Time",
+    ylab="Time to Fetch and Validate (minutes)")
 dev.off()
