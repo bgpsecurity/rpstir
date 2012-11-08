2e7bcfecd8f6cd7d3a43eeec373e1d80dd4830a7
David Mandelberg <dmandelb@bbn.com>
2012-11-07 22:03:20 -0500

    update parse-results.sh to support tab-separated tables

--- bin/rpki-statistics/for-each-run-helpers/parse-results.sh
+++ bin/rpki-statistics/for-each-run-helpers/parse-results.sh
@@ -2,8 +2,7 @@
 
 # Used to answer questions such as: "What % of certs/roas/etc... are invalid?"
 
-# If one argument is specified, print that field.
-# If two arguments are specified, compute the ratio of field $1 to field $2 in the results file.
+# See usage() below for how to call this script.
 
 
 get_field_value () {
@@ -29,17 +28,44 @@ get_field_value () {
     }
 }
 
-
-if test $# -eq 1; then
-    get_field_value "$1"
-elif test $# -eq 2; then
-    NUMERATOR=`get_field_value "$1"`
-    DENOMINATOR=`get_field_value "$2"`
-
-    awk "BEGIN {print $NUMERATOR * 1.0 / $DENOMINATOR}"
-else
+usage () {
     echo >&2 "Usage:"
-    echo >&2 "    $0 results-field"
-    echo >&2 "    $0 results-field-for-numerator results-field-for-denominator"
+    echo >&2
+    echo >&2 "    $0 all <results-field1> [<results-field2> ...]"
+    echo >&2 "        print all the fields in order, separated by tabs"
+    echo >&2
+    echo >&2 "    $0 ratio <results-field-for-numerator> <results-field-for-denominator>"
+    echo >&2 "        print the ratio of the two fields"
     exit 1
-fi
+}
+
+METHOD="$1"
+shift || usage
+case "$METHOD" in
+    all)
+        test $# -ge 1 || usage
+
+        # Don't print anything before the first field,
+        # but do print "\t" before each subsequent field.
+        char_before_datum=""
+
+        for field in "$@"; do
+            printf "${char_before_datum}%s" "$(get_field_value "$field")"
+            char_before_datum="\t"
+        done
+        printf "\n"
+        ;;
+
+    ratio)
+        test $# -eq 2 || usage
+
+        NUMERATOR=`get_field_value "$1"`
+        DENOMINATOR=`get_field_value "$2"`
+
+        awk "BEGIN {print $NUMERATOR * 1.0 / $DENOMINATOR}"
+        ;;
+
+    *)
+        usage
+        ;;
+esac
