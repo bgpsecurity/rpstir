f5a7a750ef50636f85eabb98b504bb176c10d308
David Mandelberg <dmandelb@bbn.com>
2012-11-07 23:54:36 -0500

    various fixes

--- bin/rpki-statistics/plots/total-objects-over-time.r
+++ bin/rpki-statistics/plots/total-objects-over-time.r
@@ -1,13 +1,16 @@
 data <- read.table('total-objects-over-time.dat', sep="\t", header=TRUE)
-data$Start <- as.POSIXlt(read.table('times.dat', sep="\t", header=TRUE)$Start)
 
-x <- data$Start
+times <- read.table('times.dat', sep="\t", header=TRUE)
+times$Start <- as.POSIXlt(times$Start)
+times$End <- as.POSIXlt(times$End)
+
+x <- times$Start + difftime(times$End, times$Start) / 2
 y <- data$CACerts + data$CRLs + data$ROAs + data$MFTs
 
 png('total-objects-over-time.png', width=1600, height=1200, pointsize=24)
-plot(x, y, type="0",
+plot(x, y, type="o",
     main="Total Number of Objects",
-    xlab="Start Time (UTC)",
+    xlab="Time (UTC)",
     ylab="Number of Objects",
     ylim=c(0,max(y)))
 dev.off()
