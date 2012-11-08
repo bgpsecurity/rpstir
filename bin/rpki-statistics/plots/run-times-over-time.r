662b79773e77208c2c2529cbf60ae482fe94c314
David Mandelberg <dmandelb@bbn.com>
2012-11-07 22:05:14 -0500

    note time zone

--- bin/rpki-statistics/plots/run-times-over-time.r
+++ bin/rpki-statistics/plots/run-times-over-time.r
@@ -4,6 +4,6 @@ data$Start <- as.POSIXlt(read.table('times.dat', sep="\t", header=TRUE)$Start)
 png('run-times-over-time.png', width=1600, height=1200, pointsize=24)
 plot(data$Start, data$Duration/60, type="o",
     main="Combined Fetch and Validation Times",
-    xlab="Start Time",
+    xlab="Start Time (UTC)",
     ylab="Time to Fetch and Validate (minutes)")
 dev.off()
