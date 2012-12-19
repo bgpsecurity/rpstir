0fba33701d966b2105cab3b8fd180b4b6218478d
David Mandelberg <dmandelb@bbn.com>
2012-12-19 15:36:39 -0500

    more renaming and moving from bindir to pkglibexecdir

--- mk/rpki-local-ta.mk
+++ mk/rpki-local-ta.mk
@@ -1,22 +1,23 @@
-bin_PROGRAMS += bin/rpki-local-ta/dumpIPAddr
+pkglibexec_PROGRAMS += bin/rpki-local-ta/dumpIPAddr
 
 bin_rpki_local_ta_dumpIPAddr_LDADD = \
 	$(LDADD_LIBRPKIOBJECT)
 
 
-bin_PROGRAMS += bin/rpki-local-ta/proofreader
+pkglibexec_PROGRAMS += bin/rpki-local-ta/proofreader
+PACKAGE_NAME_BINS += proofreader
 
 bin_rpki_local_ta_proofreader_LDADD = \
 	$(LDADD_LIBRPKI)
 
 
-bin_PROGRAMS += bin/rpki-local-ta/test_cert
+pkglibexec_PROGRAMS += bin/rpki-local-ta/test_cert
 
 bin_rpki_local_ta_test_cert_LDADD = \
 	$(LDADD_LIBRPKIOBJECT)
 
 
-bin_PROGRAMS += bin/rpki-local-ta/testrpwork
+pkglibexec_PROGRAMS += bin/rpki-local-ta/testrpwork
 
 bin_rpki_local_ta_testrpwork_LDADD = \
 	$(LDADD_LIBRPKI)
