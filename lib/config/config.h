67353b7bbeed8d1533c7b4e3d96cec60db808fb4
David Mandelberg <dmandelb@bbn.com>
2013-01-08 12:39:47 -0500

    add a summary of how to access config values

--- lib/config/config.h
+++ lib/config/config.h
@@ -36,6 +36,27 @@ enum config_key {
     CONFIG_NUM_OPTIONS
 };
 
+
+/**
+    The below macro calls generate helper functions to access the configuration
+    values. See the definitions of each macro in lib/configlib/configlib.h for
+    more detail, but here's a summary of how to access some of the below
+    options:
+
+        const char * root_dir = CONFIG_ROOT_DIR_get();
+
+        uint16_t rpki_port = CONFIG_RPKI_PORT_get();
+
+        for (size_t i = 0;
+            i < config_get_length(CONFIG_RPKI_EXTRA_PUBLICATION_POINTS);
+            ++i)
+        {
+            const char * rpki_extra_pub_pt =
+                CONFIG_RPKI_EXTRA_PUBLICATION_POINTS_get(i);
+            printf("extra publication point: %s\n", rpki_extra_pub_pt);
+        }
+*/
+
 CONFIG_GET_HELPER(CONFIG_ROOT_DIR, char)
 CONFIG_GET_HELPER_DEREFERENCE(CONFIG_RPKI_PORT, uint16_t)
 CONFIG_GET_HELPER(CONFIG_DATABASE, char)
