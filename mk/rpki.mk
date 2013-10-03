f15c65b903a302ad628639cd86f6294f30dccf8b
Edric Barnes <ebarnes@bbn.com>
2013-10-03 17:46:43 -0400

    Added 625 IPv6ExtraSubPrefixInPrefixMiddle

--- mk/rpki.mk
+++ mk/rpki.mk
@@ -350,6 +350,7 @@ EXTRA_DIST += \
 	tests/conformance/raw/keys/goodROAIPv4OnlyPrefixesInPrefixesMiddle.ee.p15 \
 	tests/conformance/raw/keys/goodROAIPv4PrefixEqualPrefix.ee.p15 \
 	tests/conformance/raw/keys/goodROAIPv4PrefixesEqualPrefixes.ee.p15 \
+	tests/conformance/raw/keys/goodROAIPv6ExtraSubPrefixInPrefixMiddle.ee.p15 \
 	tests/conformance/raw/keys/goodROAIPv6PrefixEqualPrefix.ee.p15 \
 	tests/conformance/raw/keys/goodROAIPv6PrefixesEqualPrefixes.ee.p15 \
 	tests/conformance/raw/keys/goodROANothingWrong.ee.p15 \
@@ -1211,6 +1212,10 @@ EXTRA_DIST += \
 	tests/conformance/raw/patches/goodROAIPv4PrefixesEqualPrefixes.stage1.patch \
 	tests/conformance/raw/patches/goodROAIPv4PrefixesEqualPrefixes.stage2.patch \
 	tests/conformance/raw/patches/goodROAIPv4PrefixesEqualPrefixes.stage3.patch \
+	tests/conformance/raw/patches/goodROAIPv6ExtraSubPrefixInPrefixMiddle.ee.stage0.patch \
+	tests/conformance/raw/patches/goodROAIPv6ExtraSubPrefixInPrefixMiddle.stage1.patch \
+	tests/conformance/raw/patches/goodROAIPv6ExtraSubPrefixInPrefixMiddle.stage2.patch \
+	tests/conformance/raw/patches/goodROAIPv6ExtraSubPrefixInPrefixMiddle.stage3.patch \
 	tests/conformance/raw/patches/goodROAIPv6PrefixEqualPrefix.ee.stage0.patch \
 	tests/conformance/raw/patches/goodROAIPv6PrefixEqualPrefix.stage1.patch \
 	tests/conformance/raw/patches/goodROAIPv6PrefixEqualPrefix.stage2.patch \
