e2b2b197f617471a78c99e662eb924dfa54665e6
Edric Barnes <ebarnes@bbn.com>
2013-10-14 16:54:59 -0400

    Added 699 IPv6OnlyPfxBetweenRangePfxNoGaps

--- mk/rpki.mk
+++ mk/rpki.mk
@@ -343,6 +343,7 @@ EXTRA_DIST += \
 	tests/conformance/raw/keys/badROAIPv6OnlyPfxBelowRangeNoGap.ee.p15 \
 	tests/conformance/raw/keys/badROAIPv6OnlyPfxBetweenPfxPfxNoGaps.ee.p15 \
 	tests/conformance/raw/keys/badROAIPv6OnlyPfxBetweenPfxRangeNoGaps.ee.p15 \
+	tests/conformance/raw/keys/badROAIPv6OnlyPfxBetweenRangePfxNoGaps.ee.p15 \
 	tests/conformance/raw/keys/badROAIPv6OnlyPfxSpanPfxes.ee.p15 \
 	tests/conformance/raw/keys/badROAIPv6OnlyPfxSupersetHighPfx.ee.p15 \
 	tests/conformance/raw/keys/badROAIPv6OnlyPfxSupersetLowPfx.ee.p15 \
@@ -1200,6 +1201,10 @@ EXTRA_DIST += \
 	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenPfxRangeNoGaps.stage1.patch \
 	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenPfxRangeNoGaps.stage2.patch \
 	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenPfxRangeNoGaps.stage3.patch \
+	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenRangePfxNoGaps.ee.stage0.patch \
+	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenRangePfxNoGaps.stage1.patch \
+	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenRangePfxNoGaps.stage2.patch \
+	tests/conformance/raw/patches/badROAIPv6OnlyPfxBetweenRangePfxNoGaps.stage3.patch \
 	tests/conformance/raw/patches/badROAIPv6OnlyPfxSpanPfxes.ee.stage0.patch \
 	tests/conformance/raw/patches/badROAIPv6OnlyPfxSpanPfxes.stage1.patch \
 	tests/conformance/raw/patches/badROAIPv6OnlyPfxSpanPfxes.stage2.patch \
