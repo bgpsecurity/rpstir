cb8286fcbe6abd20a7be62a977757ef8a8a06b1d
Edric Barnes <ebarnes@bbn.com>
2013-10-17 15:54:06 -0400

    Added 710 IPv6PfxesEqualRanges

--- mk/rpki.mk
+++ mk/rpki.mk
@@ -391,6 +391,7 @@ EXTRA_DIST += \
 	tests/conformance/raw/keys/goodROAIPv6PfxEqualPfx.ee.p15 \
 	tests/conformance/raw/keys/goodROAIPv6PfxesEqualPfxes.ee.p15 \
 	tests/conformance/raw/keys/goodROAIPv6PfxesEqualRange.ee.p15 \
+	tests/conformance/raw/keys/goodROAIPv6PfxesEqualRanges.ee.p15 \
 	tests/conformance/raw/keys/goodROANothingWrong.ee.p15 \
 	tests/conformance/raw/patches/NAMSeqNameSer-goodCRLMatch.crl.stage0.patch \
 	tests/conformance/raw/patches/NAMSeqNameSer-goodCertMatch.cer.stage0.patch \
@@ -1414,6 +1415,10 @@ EXTRA_DIST += \
 	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRange.stage1.patch \
 	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRange.stage2.patch \
 	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRange.stage3.patch \
+	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRanges.ee.stage0.patch \
+	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRanges.stage1.patch \
+	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRanges.stage2.patch \
+	tests/conformance/raw/patches/goodROAIPv6PfxesEqualRanges.stage3.patch \
 	tests/conformance/raw/patches/goodROANothingWrong.ee.stage0.patch \
 	tests/conformance/raw/patches/goodROANothingWrong.stage1.patch \
 	tests/conformance/raw/patches/goodROANothingWrong.stage2.patch \
