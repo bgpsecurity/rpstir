585547a9a1ccc225091f83050362c8158bd75515
David Mandelberg <dmandelb@bbn.com>
2013-12-31 18:54:02 -0500

    remove deprecated/

--- deprecated/testing/unittest/Phase2/unpem.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-for inFile in $*; do
-  outFile=`echo ${inFile} | sed -e "s/.pem//"`
-  [ "x${outFile}" = "x${inFile}" ] && \
-    continue
-   echo "${inFile} ==> ${outFile}"
-  (./un64 ${inFile} && mv out.bin ${outFile}) > /dev/null 2>&1
-done
