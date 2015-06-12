4ffe704fca6879cb8b2b2df2295b485f77ec50ae
Matt Carr <mcarr@bbn.com>
2015-06-12 13:54:35 -0400

    Change privileges to user/group specified in rpstir.conf
    1. Parse user and group with passwd and group config type converters
    
    2. Support either user/group name or UID/GID
    
    3. If user or group are commented out or blank in rpstir.conf,
    do not attempt to change privileges

--- mk/libconfiglib.mk
+++ mk/libconfiglib.mk
@@ -14,22 +14,16 @@ lib_configlib_libconfiglib_a_SOURCES = \
 	lib/configlib/types/bool.h \
 	lib/configlib/types/enum.c \
 	lib/configlib/types/enum.h \
-<<<<<<< HEAD
-=======
 	lib/configlib/types/group.c \
 	lib/configlib/types/group.h \
 	lib/configlib/types/passwd.c \
 	lib/configlib/types/passwd.h \
->>>>>>> Drop root privileges
 	lib/configlib/types/path.c \
 	lib/configlib/types/path.h \
 	lib/configlib/types/sscanf.c \
 	lib/configlib/types/sscanf.h \
 	lib/configlib/types/string_cvt.c \
-	lib/configlib/types/string_cvt.h \
-	lib/configlib/types/passwd.c \
-	lib/configlib/types/passwd.h
-
+	lib/configlib/types/string_cvt.h
 
 check_PROGRAMS += lib/configlib/tests/good
 
