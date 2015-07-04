599ef1cc02f77366067728941fa125bffcc5b014
Richard Hansen <rhansen@bbn.com>
2015-07-04 19:34:02 -0400

    configure.ac, autogen.sh: wrap long lines where possible

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-#This builds all necessary Makefile.in's and other necessary files and then generates
-# the configure script.
+# This builds all necessary Makefile.in's and other necessary files
+# and then generates the configure script.
 autoreconf --force --install --verbose
