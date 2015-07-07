b6b59dd63d75a32b0916719a4d456883329c5d2f
Richard Hansen <rhansen@bbn.com>
2015-07-06 23:11:28 -0400

    autogen.sh: avoid inconsistent 'set -e'
    Different shells interpret the '-e' argument differently; avoid it.
    (Also, '-e' is unnecessary here.)

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 # This builds all necessary Makefile.in's and other necessary files
 # and then generates the configure script.
 autoreconf --force --install --verbose -Wall
