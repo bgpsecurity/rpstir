7a9ef313f69e1b2cf68a56e84b87543a68803885
Richard Hansen <rhansen@bbn.com>
2015-07-06 23:11:34 -0400

    autogen.sh: avoid a fork

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This builds all necessary Makefile.in's and other necessary files
 # and then generates the configure script.
-autoreconf --force --install --verbose -Wall
+exec autoreconf --force --install --verbose -Wall
