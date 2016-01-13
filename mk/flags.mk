442c24a3e40c12dc689385014ccfe189cd84f4ad
Richard Hansen <rhansen@bbn.com>
2016-01-12 22:10:57 -0500

    don't modify user flag variables in configure.ac
    Modifying CFLAGS, CPPFLAGS, LDFLAGS, LIBS, etc. can cause problems
    when the user wants to temporarily change them when building (e.g.,
    'make CFLAGS="-Werror"').  See:
    https://www.gnu.org/software/automake/manual/html_node/User-Variables.html
    
    This change also prevents flags added by previous tests from
    interfering with later tests.  In particular, this stops the flags
    added for MySQL from breaking the pthread flag detection, so
    '-pthread' is now properly passed to the compiler.

--- mk/flags.mk
+++ mk/flags.mk
@@ -3,15 +3,24 @@ PACKAGE_SYS_CONF_FILE = $(pkgsysconfdir)/$(PACKAGE_NAME).conf
 
 
 AM_CFLAGS = \
+	$(CONFIGURE_CFLAGS) \
 	-Wall \
 	-Wextra \
 	-g
 
 AM_CPPFLAGS = \
+	$(CONFIGURE_CPPFLAGS) \
 	-I$(top_builddir)/lib \
 	-I$(top_srcdir)/lib \
 	-DABS_TOP_SRCDIR='"@abs_top_srcdir@"' \
 	-DPACKAGE_SYS_CONF_FILE='"$(PACKAGE_SYS_CONF_FILE)"'
+
+AM_LDFLAGS = \
+	$(CONFIGURE_LDFLAGS)
+
+# there is no AM_LIBS variable for some reason
+LIBS += \
+	$(CONFIGURE_LIBS)
 
 AM_ETAGSFLAGS = \
 	--declarations
