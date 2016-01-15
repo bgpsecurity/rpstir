87235454c1e702e215863eac7a5e811818aeb3f9
Richard Hansen <rhansen@bbn.com>
2016-01-15 13:56:50 -0500

    rename subsystem test step scripts (step 4/7)
    ...to fill in gaps left by deleted test step scripts.
    
    The renaming of the test scripts is spread out over several commits to
    make the changes easier to review.

--- mk/rpki.mk
+++ mk/rpki.mk
@@ -1938,7 +1938,7 @@ check_SCRIPTS += \
 	tests/subsystem/step1.2.tap \
 	tests/subsystem/step1.3.tap \
 	tests/subsystem/step1.4.tap \
-	tests/subsystem/step1.7.tap \
+	tests/subsystem/step1.5.tap \
 	tests/subsystem/step1.8.tap \
 	tests/subsystem/step1.9.tap \
 	tests/subsystem/step2.1.tap \
@@ -1953,7 +1953,7 @@ check_SCRIPTS += \
 	tests/subsystem/step3.2.tap \
 	tests/subsystem/step3.3.tap \
 	tests/subsystem/step3.4.tap \
-	tests/subsystem/step3.6.tap \
+	tests/subsystem/step3.5.tap \
 	tests/subsystem/step3.7.tap \
 	tests/subsystem/step3.8.tap \
 	tests/subsystem/step3.9.tap
@@ -1963,7 +1963,7 @@ MK_SUBST_FILES_EXEC += \
 	tests/subsystem/step1.2.tap \
 	tests/subsystem/step1.3.tap \
 	tests/subsystem/step1.4.tap \
-	tests/subsystem/step1.7.tap \
+	tests/subsystem/step1.5.tap \
 	tests/subsystem/step1.8.tap \
 	tests/subsystem/step1.9.tap \
 	tests/subsystem/step2.1.tap \
@@ -1978,7 +1978,7 @@ MK_SUBST_FILES_EXEC += \
 	tests/subsystem/step3.2.tap \
 	tests/subsystem/step3.3.tap \
 	tests/subsystem/step3.4.tap \
-	tests/subsystem/step3.6.tap \
+	tests/subsystem/step3.5.tap \
 	tests/subsystem/step3.7.tap \
 	tests/subsystem/step3.8.tap \
 	tests/subsystem/step3.9.tap
@@ -1987,7 +1987,7 @@ tests/subsystem/step1.1.tap: $(srcdir)/tests/subsystem/step1.1.tap.in
 tests/subsystem/step1.2.tap: $(srcdir)/tests/subsystem/step1.2.tap.in
 tests/subsystem/step1.3.tap: $(srcdir)/tests/subsystem/step1.3.tap.in
 tests/subsystem/step1.4.tap: $(srcdir)/tests/subsystem/step1.4.tap.in
-tests/subsystem/step1.7.tap: $(srcdir)/tests/subsystem/step1.7.tap.in
+tests/subsystem/step1.5.tap: $(srcdir)/tests/subsystem/step1.5.tap.in
 tests/subsystem/step1.8.tap: $(srcdir)/tests/subsystem/step1.8.tap.in
 tests/subsystem/step1.9.tap: $(srcdir)/tests/subsystem/step1.9.tap.in
 tests/subsystem/step2.1.tap: $(srcdir)/tests/subsystem/step2.1.tap.in
@@ -2002,7 +2002,7 @@ tests/subsystem/step3.1.tap: $(srcdir)/tests/subsystem/step3.1.tap.in
 tests/subsystem/step3.2.tap: $(srcdir)/tests/subsystem/step3.2.tap.in
 tests/subsystem/step3.3.tap: $(srcdir)/tests/subsystem/step3.3.tap.in
 tests/subsystem/step3.4.tap: $(srcdir)/tests/subsystem/step3.4.tap.in
-tests/subsystem/step3.6.tap: $(srcdir)/tests/subsystem/step3.6.tap.in
+tests/subsystem/step3.5.tap: $(srcdir)/tests/subsystem/step3.5.tap.in
 tests/subsystem/step3.7.tap: $(srcdir)/tests/subsystem/step3.7.tap.in
 tests/subsystem/step3.8.tap: $(srcdir)/tests/subsystem/step3.8.tap.in
 tests/subsystem/step3.9.tap: $(srcdir)/tests/subsystem/step3.9.tap.in
