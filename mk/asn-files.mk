3c788f7d5f17421fd61a711676ac0d9d3144e8af
Richard Hansen <rhansen@bbn.com>
2016-02-24 21:07:31 -0500

    add logging to determine which command is failing

--- mk/asn-files.mk
+++ mk/asn-files.mk
@@ -29,25 +29,31 @@ ASN_GENERATION_DEPS = \
 # http://www.gnu.org/software/automake/manual/html_node/Multiple-Outputs.html
 # for an overview.
 $(ASN_C_FILES) $(ASN_H_FILES): $(ASN_GENERATION_DEPS)
-	$(AM_V_GEN)tmpdir=`$(MKTEMP_DIR)` && \
-	base=`echo "$(@F)" | $(SED) "s/\\.[ch]\$$//"` && \
+	$(AM_V_GEN)pecho() { printf %s\\n "$$*"; } ; \
+	log() { pecho "$$@"; } ; \
+	error() { log "ERROR: $$@" >&2; } ; \
+	fatal() { error "$$@"; exit 1; } ; \
+	try() { "$$@" || fatal "'$$@' failed"; } ; \
+	tmpdir=$$(try $(MKTEMP_DIR)) || exit 1; \
+	base=$$(echo "$(@F)" | try $(SED) "s/\\.[ch]\$$//") || exit 1; \
 	for f in $(ASN_BUILT_FILES); do \
-		dir=`dirname "$$f"` && \
-		mkdir -p "$$tmpdir/$$dir" && \
-		cp "$$f" "$$tmpdir/$$dir"; \
-	done && \
+		dir=$$(try dirname "$$f") || exit 1; \
+		try mkdir -p "$$tmpdir/$$dir"; \
+		try cp "$$f" "$$tmpdir/$$dir"; \
+	done; \
 	for f in $(ASN_SOURCE_FILES); do \
-		dir=`dirname "$$f"` && \
-		mkdir -p "$$tmpdir/$$dir" && \
-		cp "$(srcdir)/$$f" "$$tmpdir/$$dir"; \
-	done && \
-	cd "$$tmpdir/$(@D)" && \
+		dir=$$(try dirname "$$f") || exit 1; \
+		try mkdir -p "$$tmpdir/$$dir"; \
+		try cp "$(srcdir)/$$f" "$$tmpdir/$$dir"; \
+	done; \
+	try cd "$$tmpdir/$(@D)"; \
 	TEST_LOG_NAME="$(@F)" \
 		TEST_LOG_DIR="$(abs_builddir)/$(@D)" \
 		STRICT_CHECKS=0 \
 		$(LOG_COMPILER) \
-		$(abs_top_builddir)/lib/casn/asn_gen/asn_gen "$${base}.asn" && \
-	cd "$(abs_builddir)" && \
-	mkdir -p "$(@D)" && \
-	cp "$$tmpdir/$@" "$(@D)" && \
-	rm -rf "$$tmpdir"
+		$(abs_top_builddir)/lib/casn/asn_gen/asn_gen "$${base}.asn" \
+		|| fatal "'$(LOG_COMPILER) asn_gen $${base}.asn' failed"; \
+	try cd "$(abs_builddir)"; \
+	try mkdir -p "$(@D)"; \
+	try cp "$$tmpdir/$@" "$(@D)"; \
+	try rm -rf "$$tmpdir"
