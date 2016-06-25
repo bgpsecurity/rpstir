7889553b16aac86c8ab539ffaa0fece4357e65d5
Richard Hansen <rhansen@bbn.com>
2016-06-25 17:18:14 -0400

    log the contents of casn_err_struct on error

--- lib/casn/casn_error.c
+++ lib/casn/casn_error.c
@@ -20,6 +20,10 @@ default_casn_error_handler(
     const char *msg)
 {
     LOG(LOG_ERR, "casn error #%d: %s", num, msg);
+    LOG(LOG_ERR, "  casn error details: errnum=%i", casn_err_struct.errnum);
+    LOG(LOG_ERR, "  casn error details: asn_map_string=%s",
+        casn_err_struct.asn_map_string);
+    LOG(LOG_ERR, "  casn error details: casnp=%p", casn_err_struct.casnp);
 }
 
 casn_error_callback *casn_error = &default_casn_error_handler;
