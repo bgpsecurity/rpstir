41bff8d1f571980753a33bdd614591fa790fc8cd
cpusoft <cpusoft@live.com>
2018-10-18 16:24:45 +0800

    update crontab sh

--- doc/docker/dockerbuilder/crontab_rpstir.sh
+++ doc/docker/dockerbuilder/crontab_rpstir.sh
@@ -2,5 +2,6 @@ dayt="$(date +%Y%m%d)"
 startt="$(date +%Y%m%d-%H:%M:%S)"
 echo $startt >> /usr/local/var/log/rpstir/crontab.log.$dayt
 rpstir-synchronize >>  /usr/local/var/log/rpstir/crontab.log.$dayt 2>&1 
+rpstir-rpki-rtr-update >>  /usr/local/var/log/rpstir/crontab.log.$dayt 2>&1 
 endt="$(date +%Y%m%d-%H:%M:%S)"
 echo $endt >>  /usr/local/var/log/rpstir/crontab.log.$dayt
