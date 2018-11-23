2a5e9243dc7e8530fe8b89658317851e940bc89e
cpusoft <cpusoft@live.com>
2018-11-23 09:26:54 +0800

    update

--- doc/docker/docker_init_run_centos6.9.sh
+++ doc/docker/docker_init_run_centos6.9.sh
@@ -1,8 +1,8 @@
 docker volume create --name=mysqldatadir
 mkdir -p  /root/dockervolume
-mkdir -p /root/dockervolume/rpstir/cache  /root/dockervolume/rpstir/log
+mkdir -p /root/dockervolume/rpstir/cache /root/dockervolume/rpstir/cache-valid /root/dockervolume/rpstir/log
 rm -rf /root/dockervolume/mysql
 ln -s  /var/lib/docker/volumes/mysqldatadir/_data  /root/dockervolume/mysql
-docker run -itd --privileged -p 13306:3306   -p 11234:1234 -v mysqldatadir:/var/lib/mysql  -v /root/dockervolume/rpstir/cache:/usr/local/var/cache/rpstir  -v /root/dockervolume/rpstir/log:/usr/local/var/log/rpstir     -v /etc/localtime:/etc/localtime --name=rpstir_docker_centos6.9   rpstir/rpstir_docker_centos6.9 
+docker run -itd --privileged -p 13306:3306   -p 11234:1234 -v mysqldatadir:/var/lib/mysql  -v /root/dockervolume/rpstir/cache:/usr/local/var/cache/rpstir  -v /root/dockervolume/rpstir/log:/usr/local/var/log/rpstir  -v /root/dockervolume/rpstir/cache-valid:/usr/local/var/cache/rpstir-tmp   -v /etc/localtime:/etc/localtime --name=rpstir_docker_centos6.9   rpstir/rpstir_docker_centos6.9 
 sleep 2s
 docker ps
