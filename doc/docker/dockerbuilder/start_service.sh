d8da87bf84ff7ebb5abeae47081c39afed44c087
cpusoft <cpusoft@live.com>
2019-03-19 11:45:03 +0800

    update Dockerfile

--- doc/docker/dockerbuilder/start_service.sh
+++ doc/docker/dockerbuilder/start_service.sh
@@ -2,7 +2,8 @@
 service crond restart
 service mysqld restart
 cd /root/rpki/rpstir/bin/
-./rpki-rtr-daemon
+./rpki-rtr-daemon &
 cd /root/rpki/rpki-transfer/bin/
 ./startup.sh &
+cd /root/rpki
 /bin/bash 
